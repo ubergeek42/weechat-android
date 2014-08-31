57db781ea728c48f338aa4d69166683c1cda5d23
oakkitten <7e14hz@gmail.com>
2014-09-01 02:37:05 +0300

    more consistent nicklist on 2.x

--- weechat-android/src/main/java/com/ubergeek42/WeechatAndroid/WeechatActivity.java
+++ weechat-android/src/main/java/com/ubergeek42/WeechatAndroid/WeechatActivity.java
@@ -33,7 +33,6 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.graphics.Rect;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Looper;
@@ -430,12 +429,6 @@ public class WeechatActivity extends SherlockFragmentActivity implements RelayCo
                 });
                 AlertDialog dialog = builder.create();
                 dialog.setTitle("squirrels are awesome");
-
-                // workaround for a bug in earlier versions of android where
-                // alert dialogs would display black on black or white on white
-                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB)
-                    dialog.getListView().setBackgroundColor(0xff000000);
-
                 dialog.setOnShowListener(nicklistAdapter);
                 dialog.setOnDismissListener(nicklistAdapter);
                 dialog.show();
