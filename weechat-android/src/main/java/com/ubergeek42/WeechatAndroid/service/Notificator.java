65a118272865c5421d829ad509cb51353f689c75
Mickaël Thomas <mickael9@gmail.com>
2016-12-13 19:20:09 +0100

    Add a disconnect / stop connecting action in the main notification.

--- weechat-android/src/main/java/com/ubergeek42/WeechatAndroid/service/Notificator.java
+++ weechat-android/src/main/java/com/ubergeek42/WeechatAndroid/service/Notificator.java
@@ -82,6 +82,17 @@ public class Notificator {
         if (P.notificationTicker)
             builder.setTicker(tickerText);
 
+        String disconnectText = context.getString(relay.state.contains(AUTHENTICATED) ? R.string.disconnect : R.string.stop_connecting);
+
+        builder.addAction(
+                android.R.drawable.ic_menu_close_clear_cancel, disconnectText,
+                PendingIntent.getService(
+                    context, 0,
+                    new Intent(RelayService.ACTION_STOP, null, context, RelayService.class),
+                    0
+                )
+        );
+
         Notification notification = builder.build();
         notification.flags |= Notification.FLAG_ONGOING_EVENT;
 
