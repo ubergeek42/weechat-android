02adc924c86299233cfe4dc179e5a745b6024b17
oakkitten <oakkitten@users.noreply.github.com>
2020-09-07 21:27:47 +0100

    spin indeterminately when progress is 1f and waiting for server response

--- app/src/main/java/com/ubergeek42/WeechatAndroid/fragments/BufferFragment.java
+++ app/src/main/java/com/ubergeek42/WeechatAndroid/fragments/BufferFragment.java
@@ -482,11 +482,14 @@ public class BufferFragment extends Fragment implements BufferEye, OnKeyListener
         }
     }
 
+    // show indeterminate progress in 2 cases:
+    // * in the beginning, when we are waiting to start uploading (useless?)
+    // * in the end, when we are waiting for the server to produce a response
     @MainThread void setUploadProgress(float ratio) {
         if (ratio < 0) {
             uploadProgressBar.setIndeterminate(false);
             uploadProgressBar.setProgress(0);
-        } else if (ratio < 0.05f) {
+        } else if (ratio < 0.05f || ratio >= 1f) {
             uploadProgressBar.setIndeterminate(true);
         } else {
             uploadProgressBar.setIndeterminate(false);
