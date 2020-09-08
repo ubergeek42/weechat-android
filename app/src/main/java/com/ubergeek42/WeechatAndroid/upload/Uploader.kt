5a7026d883df7aa0b0c2bf387b0cad0b3958415d
oakkitten <oakkitten@users.noreply.github.com>
2020-09-08 13:26:11 +0100

    refactor uploader more, add global job manager

--- app/src/main/java/com/ubergeek42/WeechatAndroid/upload/Uploader.kt
+++ app/src/main/java/com/ubergeek42/WeechatAndroid/upload/Uploader.kt
@@ -1,9 +1,12 @@
 package com.ubergeek42.WeechatAndroid.upload
 
+import android.net.Uri
+import androidx.annotation.MainThread
 import okhttp3.*
 import okio.BufferedSink
 import okio.IOException
 import okio.source
+import kotlin.concurrent.thread
 
 
 interface ProgressListener {
@@ -14,32 +17,40 @@ interface ProgressListener {
 }
 
 
-const val FORM_FIlE_NAME = "file"
-const val UPLOAD_URI = "https://x0.at"
-const val SEGMENT_SIZE = 4096L
+private const val FORM_FIlE_NAME = "file"
+private const val UPLOAD_URI = "https://x0.at"
+private const val SEGMENT_SIZE = 4096L
 
 
 private val client = OkHttpClient()
 
 
 class Uploader(
-        private val suri: Suri,
-        private val progressListener: ProgressListener
+    private val suri: Suri
 ) {
-    var call: Call? = null
+    private val listeners = mutableSetOf<ProgressListener>()
+    private var call: Call? = null
 
-    fun upload() {
+    private fun upload() {
         try {
             call = prepare()
-            progressListener.onStarted()
+            jobs.lock {
+                listeners.forEach { it.onStarted() }
+            }
             val response = execute()
-            progressListener.onDone(response)
+            jobs.lock {
+                listeners.forEach { it.onDone(response) }
+                remove(suri.uri)
+            }
         } catch (e: Exception) {
-            progressListener.onFailure(e)
+            jobs.lock {
+                listeners.forEach { it.onFailure(e) }
+                remove(suri.uri)
+            }
         }
     }
 
-    fun cancel() {
+    private fun cancel() {
         call?.cancel()
     }
 
@@ -78,7 +89,9 @@ class Uploader(
 
                 suri.getInputStream().source().use {
                     while (true) {
-                        progressListener.onProgress(totalRead, suri.fileSize)
+                        jobs.lock {
+                            listeners.forEach { l -> l.onProgress(totalRead, suri.fileSize) }
+                        }
 
                         val read = it.read(sink.buffer, SEGMENT_SIZE)
                         if (read == -1L) return
@@ -91,4 +104,42 @@ class Uploader(
             }
         }
     }
+
+    companion object Jobs {
+        var counter = 0
+        val jobs = mutableMapOf<Uri, Uploader>()
+
+        @MainThread fun upload(suri: Suri, listener: ProgressListener) {
+            jobs.lock {
+                var uploader = jobs[suri.uri]
+                if (uploader != null) {
+                    uploader.listeners.add(listener)
+                    listener.onStarted()
+                } else {
+                    uploader = Uploader(suri)
+                    uploader.listeners.add(listener)
+                    jobs[suri.uri] = uploader
+                    thread(name = "u-" + counter++) { uploader.upload() }
+                }
+            }
+        }
+
+        @MainThread fun cancel(suri: Suri) {
+            jobs.lock {
+                jobs[suri.uri]?.cancel()
+            }
+        }
+    }
 }
+
+private inline fun <T : Any> T.lock(func: (T.() -> Unit)) {
+    synchronized (this) {
+        func(this)
+    }
+}
+
+// private fun <T> Iterable<T>.forEachLocked(func: (T.() -> Unit)) {
+//     this.lock {
+//         forEach(func)
+//     }
+// }
