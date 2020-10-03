33b1a0bb1a87c02fc006150b22ce528411f8135d
oakkitten <oakkitten@users.noreply.github.com>
2020-10-03 23:14:35 +0100

    remove the old know hosts preference while migrating

--- app/src/main/java/com/ubergeek42/WeechatAndroid/utils/Constants.java
+++ app/src/main/java/com/ubergeek42/WeechatAndroid/utils/Constants.java
@@ -162,5 +162,6 @@ public class Constants {
         final static public String PREF_SSH_PASS = "ssh_pass"; final public static String PREF_SSH_PASS_D = "";
         final static public String PREF_SSH_KEY = "ssh_key"; final public static String PREF_SSH_KEY_D = null;
         final static public String PREF_SSH_KEY_PASSPHRASE = "ssh_key_passphrase"; final public static String PREF_SSH_KEY_PASSPHRASE_D = null;
+        final static public String PREF_SSH_KNOWN_HOSTS = "ssh_known_hosts"; final public static String PREF_SSH_KNOWN_HOSTS_D = "";
     }
 }
