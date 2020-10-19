8805c399476aa5e338afc712ae836799f9b43208
oakkitten <oakkitten@users.noreply.github.com>
2020-10-19 03:35:26 +0100

    change a few defaults

--- app/src/main/java/com/ubergeek42/WeechatAndroid/utils/Constants.java
+++ app/src/main/java/com/ubergeek42/WeechatAndroid/utils/Constants.java
@@ -36,54 +36,87 @@ public class Constants {
     final static public String PREF_SSH_AUTHENTICATION_METHOD_PASSWORD = "password";
     final static public String PREF_SSH_AUTHENTICATION_METHOD_KEY = "key";
     final public static String PREF_SSH_AUTHENTICATION_METHOD_D = PREF_SSH_AUTHENTICATION_METHOD_PASSWORD;
-    final static public String PREF_SSH_PASSWORD = "ssh_password"; final public static String PREF_SSH_PASSWORD_D = "";
-    final static public String PREF_SSH_KEY_FILE = "ssh_key_file"; final public static String PREF_SSH_KEY_FILE_D = null;
-    final static public String PREF_SSH_SERVER_KEY_VERIFIER = "ssh_server_key_verifier"; final public static String PREF_SSH_SERVER_KEY_VERIFIER_D = "";
+    final static public String PREF_SSH_PASSWORD = "ssh_password";
+    final public static String PREF_SSH_PASSWORD_D = "";
+    final static public String PREF_SSH_KEY_FILE = "ssh_key_file";
+    final public static String PREF_SSH_KEY_FILE_D = null;
+    final static public String PREF_SSH_SERVER_KEY_VERIFIER = "ssh_server_key_verifier";
+    final public static String PREF_SSH_SERVER_KEY_VERIFIER_D = "";
 
     // relay
-    final static public String PREF_HOST = "host"; final public static String PREF_HOST_D = null;
-    final static public String PREF_PORT = "port"; final public static String PREF_PORT_D = "9001";
-    final static public String PREF_PASSWORD = "password"; final public static String PREF_PASSWORD_D = null;
+    final static public String PREF_HOST = "host";
+    final public static String PREF_HOST_D = null;
+    final static public String PREF_PORT = "port";
+    final public static String PREF_PORT_D = "9001";
+    final static public String PREF_PASSWORD = "password";
+    final public static String PREF_PASSWORD_D = null;
 
     // misc
-    final static public String PREF_LINE_INCREMENT = "line_increment"; final public static String PREF_LINE_INCREMENT_D = "100";
-    final static public String PREF_RECONNECT = "reconnect"; final public static boolean PREF_RECONNECT_D = false;
-    final static public String PREF_BOOT_CONNECT = "boot_connect"; final public static boolean PREF_BOOT_CONNECT_D = false;
-    public static final String PREF_OPTIMIZE_TRAFFIC = "optimize_traffic"; final public static boolean PREF_OPTIMIZE_TRAFFIC_D = false;
-    public final static String PREF_HOTLIST_SYNC = "hotlist_sync"; final public static boolean PREF_HOTLIST_SYNC_D = false;
+    final static public String PREF_LINE_INCREMENT = "line_increment";
+    final public static String PREF_LINE_INCREMENT_D = "300";
+    final static public String PREF_RECONNECT = "reconnect";
+    final public static boolean PREF_RECONNECT_D = true;
+    final static public String PREF_BOOT_CONNECT = "boot_connect";
+    final public static boolean PREF_BOOT_CONNECT_D = false;
+    public static final String PREF_OPTIMIZE_TRAFFIC = "optimize_traffic";
+    final public static boolean PREF_OPTIMIZE_TRAFFIC_D = false;
+    public final static String PREF_HOTLIST_SYNC = "hotlist_sync";
+    final public static boolean PREF_HOTLIST_SYNC_D = false;
 
     // ping
     final static public String PREF_PING_GROUP = "ping_group";
-    final static public String PREF_PING_ENABLED = "ping_enabled"; final public static boolean PREF_PING_ENABLED_D = true;
-    final static public String PREF_PING_IDLE = "ping_idle"; final public static String PREF_PING_IDLE_D = "300";
-    final static public String PREF_PING_TIMEOUT = "ping_timeout"; final public static String PREF_PING_TIMEOUT_D = "30";
+    final static public String PREF_PING_ENABLED = "ping_enabled";
+    final public static boolean PREF_PING_ENABLED_D = true;
+    final static public String PREF_PING_IDLE = "ping_idle";
+    final public static String PREF_PING_IDLE_D = "300";
+    final static public String PREF_PING_TIMEOUT = "ping_timeout";
+    final public static String PREF_PING_TIMEOUT_D = "30";
 
     // buffer list
     public static final String PREF_BUFFERLIST_GROUP = "bufferlist_group";
-    public static final String PREF_SORT_BUFFERS = "sort_buffers"; final public static boolean PREF_SORT_BUFFERS_D = false;
-    public static final String PREF_HIDE_HIDDEN_BUFFERS = "hide_hidden_buffers"; final public static boolean PREF_HIDE_HIDDEN_BUFFERS_D = false;
-    public static final String PREF_FILTER_NONHUMAN_BUFFERS = "filter_nonhuman_buffers"; final public static boolean PREF_FILTER_NONHUMAN_BUFFERS_D = false;
-    public static final String PREF_SHOW_BUFFER_FILTER = "show_buffer_filter"; final public static boolean PREF_SHOW_BUFFER_FILTER_D = false;
-    public static final String PREF_USE_GESTURE_EXCLUSION_ZONE = "use_gesture_exclusion_zone"; final public static boolean PREF_USE_GESTURE_EXCLUSION_ZONE_D = false;
+    public static final String PREF_SORT_BUFFERS = "sort_buffers";
+    final public static boolean PREF_SORT_BUFFERS_D = false;
+    public static final String PREF_HIDE_HIDDEN_BUFFERS = "hide_hidden_buffers";
+    final public static boolean PREF_HIDE_HIDDEN_BUFFERS_D = true;
+    public static final String PREF_FILTER_NONHUMAN_BUFFERS = "filter_nonhuman_buffers";
+    final public static boolean PREF_FILTER_NONHUMAN_BUFFERS_D = true;
+    public static final String PREF_SHOW_BUFFER_FILTER = "show_buffer_filter";
+    final public static boolean PREF_SHOW_BUFFER_FILTER_D = true;
+    public static final String PREF_USE_GESTURE_EXCLUSION_ZONE = "use_gesture_exclusion_zone";
+    final public static boolean PREF_USE_GESTURE_EXCLUSION_ZONE_D = true;
 
     // look & feel
     final static public String PREF_LOOKFEEL_GROUP = "lookfeel_group";
-    public static final String PREF_TEXT_SIZE = "text_size"; final public static String PREF_TEXT_SIZE_D = "12";
-    public static final String PREF_AUTO_HIDE_ACTIONBAR = "auto_hide_actionbar"; public static final boolean PREF_AUTO_HIDE_ACTIONBAR_D = true;
-    public static final String PREF_FILTER_LINES = "chatview_filters"; final public static boolean PREF_FILTER_LINES_D = true;
-    public static final String PREF_PREFIX_ALIGN = "prefix_align"; final public static String PREF_PREFIX_ALIGN_D = "right";
-    final static public String PREF_MAX_WIDTH = "prefix_max_width"; final public static String PREF_MAX_WIDTH_D = "7";
-    public static final String PREF_ENCLOSE_NICK = "enclose_nick"; final public static boolean PREF_ENCLOSE_NICK_D = false;
-    final static public String PREF_TIMESTAMP_FORMAT = "timestamp_format"; final public static String PREF_TIMESTAMP_FORMAT_D = "HH:mm:ss";
-    public static final String PREF_DIM_DOWN = "dim_down"; final public static boolean PREF_DIM_DOWN_D = false;
-    public static final String PREF_BUFFER_FONT = "buffer_font"; final public static String PREF_BUFFER_FONT_D = "";
-    public static final String PREF_COLOR_SCHEME_DAY = "color_scheme_day"; final public static String PREF_COLOR_SCHEME_DAY_D = "squirrely-light-theme.properties";
-    public static final String PREF_COLOR_SCHEME_NIGHT = "color_scheme_night"; final public static String PREF_COLOR_SCHEME_NIGHT_D = "squirrely-dark-theme.properties";
+    public static final String PREF_TEXT_SIZE = "text_size";
+    final public static String PREF_TEXT_SIZE_D = "16";
+    public static final String PREF_AUTO_HIDE_ACTIONBAR = "auto_hide_actionbar";
+    public static final boolean PREF_AUTO_HIDE_ACTIONBAR_D = true;
+    public static final String PREF_FILTER_LINES = "chatview_filters";
+    final public static boolean PREF_FILTER_LINES_D = true;
+    public static final String PREF_PREFIX_ALIGN = "prefix_align";
+    final public static String PREF_PREFIX_ALIGN_D = "right";
+    final static public String PREF_MAX_WIDTH = "prefix_max_width";
+    final public static String PREF_MAX_WIDTH_D = "7";
+    public static final String PREF_ENCLOSE_NICK = "enclose_nick";
+    final public static boolean PREF_ENCLOSE_NICK_D = false;
+    final static public String PREF_TIMESTAMP_FORMAT = "timestamp_format";
+    final public static String PREF_TIMESTAMP_FORMAT_D = "HH:mm:ss";
+    public static final String PREF_DIM_DOWN = "dim_down";
+    final public static boolean PREF_DIM_DOWN_D = true;
+    public static final String PREF_BUFFER_FONT = "buffer_font";
+    final public static String PREF_BUFFER_FONT_D = "";
+    public static final String PREF_COLOR_SCHEME_DAY = "color_scheme_day";
+    final public static String PREF_COLOR_SCHEME_DAY_D = "squirrely-light-theme.properties";
+    public static final String PREF_COLOR_SCHEME_NIGHT = "color_scheme_night";
+    final public static String PREF_COLOR_SCHEME_NIGHT_D = "squirrely-dark-theme.properties";
 
     // buttons
-    public final static String PREF_SHOW_SEND = "sendbtn_show"; final public static boolean PREF_SHOW_SEND_D = true;
-    public final static String PREF_SHOW_TAB = "tabbtn_show"; final public static boolean PREF_SHOW_TAB_D = true;
-    public final static String PREF_VOLUME_BTN_SIZE = "volumebtn_size"; final public static boolean PREF_VOLUME_BTN_SIZE_D = true;
+    public final static String PREF_SHOW_SEND = "sendbtn_show";
+    final public static boolean PREF_SHOW_SEND_D = true;
+    public final static String PREF_SHOW_TAB = "tabbtn_show";
+    final public static boolean PREF_SHOW_TAB_D = true;
+    public final static String PREF_VOLUME_BTN_SIZE = "volumebtn_size";
+    final public static boolean PREF_VOLUME_BTN_SIZE_D = true;
 
     // notifications
     final static public String PREF_NOTIFICATION_GROUP = "notif_group";
