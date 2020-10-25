ee1f850e98c8589d6b3804f15aba06a75f53a75a
oakkitten <oakkitten@users.noreply.github.com>
2020-10-25 01:22:12 +0000

    save to a different folder on <Q, this doesn't require new permissions

--- app/src/main/java/com/ubergeek42/WeechatAndroid/upload/FileChooser.kt
+++ app/src/main/java/com/ubergeek42/WeechatAndroid/upload/FileChooser.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.Intent.EXTRA_ALLOW_MULTIPLE
 import android.graphics.Bitmap
 import android.net.Uri
+import android.os.Build
 import android.os.Environment
 import android.provider.MediaStore
 import androidx.core.content.FileProvider
@@ -80,15 +81,20 @@ enum class Targets(override val requestCode: Int) : Target {
     },
 
     Camera(1060) {
-        override val intent get() = Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
-                                        putExtra(MediaStore.EXTRA_OUTPUT, createMediaStoreFile())
-                                    }
+        override val intent get(): Intent {
+            val uri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+                createMediaStoreFile() else createExternalFile()
+
+            return Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
+                putExtra(MediaStore.EXTRA_OUTPUT, uri)
+            }
+        }
     },
 }
 
 
 fun chooseFiles(fragment: Fragment, longClick: Boolean) {
-    val target = if (longClick) Targets.Camera else Targets.MediaStoreImages
+    val target = if (longClick) Targets.Camera else Targets.MediaStoreImagesAndVideos
     try {
         fragment.startActivityForResult(target.intent, target.requestCode)
     } catch (e: Exception) {
@@ -117,17 +123,15 @@ fun getShareObjectFromIntent(requestCode: Int, intent: Intent?): ShareObject {
         }
 
         Targets.Camera.requestCode -> {
-            val uri = currentPhotoUri
-
             // the bitmap that the intent contains is supposed to show a smaller image
             // suitable for displaying to the user. in practice, however, the intent is null.
             // in both cases the url that we provided to the picker will point at the full image
             // see https://stackoverflow.com/a/12564910/1449683
             intent?.extras?.get("data")?.also {
-                return UrisShareObject.fromCamera(it as Bitmap, uri)
+                return UrisShareObject.fromCamera(it as Bitmap, currentPhotoUri)
             }
 
-            return UrisShareObject.fromUris(listOf(uri))
+            return UrisShareObject.fromUris(listOf(currentPhotoUri))
         }
 
         else -> throw IllegalArgumentException("Unknown request code: $requestCode")
@@ -138,34 +142,37 @@ fun getShareObjectFromIntent(requestCode: Int, intent: Intent?): ShareObject {
 // we must provide a writeable path for the camera to write to. it can be:
 // * our private dir: getExternalFilesDir() -- no permissions required
 // * external dir : getExternalStoragePublicDirectory() -- permissions required
-// * external dir MediaStore.Images table -- no permissions required, api 29+
+// * external dir MediaStore.Images table -- no permissions required on api 29+.
+//   also on api < 29 we can't put files in a folder of our choosing
+//   and i'm not sure if we can pick the name
 
-// this pass is not returned back to us, so we simply save it here
+// the uri is not returned back to us, so we simply save it here
 lateinit var currentPhotoUri: Uri
 
-@Throws(IOException::class)
-private fun createTemporaryFile(): Uri {
-    val timestamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(Date())
+
+private fun createExternalFile(): Uri {
     val directory: File = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
             ?: throw IOException("Storage directory not available")
-
-    val file = File.createTempFile("WeechatAndroid_${timestamp}_", ".jpeg", directory)
+    val file = File.createTempFile("IMG_${timestamp()}_", ".jpeg", directory)
     currentPhotoUri = Uri.fromFile(file)
     return FileProvider.getUriForFile(context, context.packageName + FILE_PROVIDER_SUFFIX, file)
 }
 
 
-// todo api 29+ only!
 private fun createMediaStoreFile(): Uri {
-    val timestamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(Date())
-
     val contentValues = ContentValues()
-    contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, "WeechatAndroid_${timestamp}.jpeg")
+    contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, "IMG_${timestamp()}.jpeg")
     contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
 
-    val resolver = context.contentResolver
-    val uri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
-            ?: throw IOException("Content provider returned null")
-    currentPhotoUri = uri
-    return uri
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH,
+                Environment.DIRECTORY_PICTURES +  "/WeechatAndroid")
+    }
+
+    return (resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+            ?: throw IOException("Content provider returned null")).also {
+        currentPhotoUri = it
+    }
 }
+
+private fun timestamp() = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(Date())
