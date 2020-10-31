faba7b876048b1fb15098c85309e0c05f47b4902
oakkitten <oakkitten@users.noreply.github.com>
2020-10-31 10:53:16 +0000

    save and restore “state” of PasswordedFilePickerPreference
    closes #475

--- app/src/main/java/androidx/preference/PasswordedFilePickerPreference.java
+++ app/src/main/java/androidx/preference/PasswordedFilePickerPreference.java
@@ -2,6 +2,9 @@ package androidx.preference;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.text.Editable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.EditText;
@@ -12,16 +15,16 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
 import com.ubergeek42.WeechatAndroid.R;
+import com.ubergeek42.WeechatAndroid.utils.Utils;
 
 public abstract class PasswordedFilePickerPreference extends FilePreference implements DialogFragmentGetter {
-    private EditText editText = null;
+    private String password = "";
 
     public PasswordedFilePickerPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
     @Override protected String saveData(@Nullable byte[] bytes) throws Exception {
-        String password = editText.getText().toString();
         return saveData(bytes, password);
     }
 
@@ -43,8 +46,62 @@ public abstract class PasswordedFilePickerPreference extends FilePreference impl
             int padding = (int) getResources().getDimension(com.ubergeek42.WeechatAndroid.R.dimen.dialog_padding);
             builder.setView(editText, padding, padding, padding, padding);
 
-            ((PasswordedFilePickerPreference) getPreference()).editText = editText;
+            editText.addTextChangedListener(new Utils.SimpleTextWatcher() {
+                @Override public void afterTextChanged(Editable s) {
+                    ((PasswordedFilePickerPreference) getPreference())
+                            .password = editText.getText().toString();
+                }
+            });
         }
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////// stuff below is boilerplate code needed to save the password. ridiculous ////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override protected Parcelable onSaveInstanceState() {
+        SavedState savedState = new SavedState(super.onSaveInstanceState());
+        savedState.password = password;
+        return savedState;
+    }
+
+    @Override protected void onRestoreInstanceState(Parcelable state) {
+        if (state instanceof SavedState) {
+            SavedState savedState = (SavedState) state;
+            super.onRestoreInstanceState(savedState.getSuperState());
+            password = savedState.password;
+        } else {
+            super.onRestoreInstanceState(state);
+        }
+    }
+
+    static class SavedState extends BaseSavedState {
+        String password = "";
+
+        public SavedState(Parcel source) {
+            super(source);
+            password = source.readString();
+        }
+
+        public SavedState(Parcelable superState) {
+            super(superState);
+        }
+
+        @Override public void writeToParcel(Parcel dest, int flags) {
+            super.writeToParcel(dest, flags);
+            dest.writeString(password);
+        }
+
+        public final static Parcelable.Creator<SavedState> CREATOR = new Parcelable.Creator<SavedState>() {
+            @Override public SavedState createFromParcel(Parcel source) {
+                return new SavedState(source);
+            }
+
+            public SavedState[] newArray(int size) {
+                return new SavedState[size];
+            }
+        };
     }
 }
 
