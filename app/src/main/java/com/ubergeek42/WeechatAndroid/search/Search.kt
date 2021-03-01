608803aef2a92153b5b3ad95dee1d4c82deb5aca
oakkitten <oakkitten@users.noreply.github.com>
2021-03-01 13:36:32 +0000

    implement basic search configuration (case sensitive, regex, source)

--- app/src/main/java/com/ubergeek42/WeechatAndroid/search/Search.kt
+++ app/src/main/java/com/ubergeek42/WeechatAndroid/search/Search.kt
@@ -1,6 +1,7 @@
 package com.ubergeek42.WeechatAndroid.search
 
 import com.ubergeek42.WeechatAndroid.relay.Line
+import java.util.*
 
 typealias MatchList = List<Long>
 
@@ -34,9 +35,49 @@ class Search(
                 return if (text.isEmpty()) {
                     Matcher { line -> line.isHighlighted }
                 } else {
-                    Matcher { line -> line.ircLikeString.contains(text) }
+                    val source = SearchConfig.source
+                    val caseSensitive = SearchConfig.caseSensitive
+                    val regex = SearchConfig.regex
+
+                    val getSource: ((Line) -> String) = when (source) {
+                        SearchConfig.Source.Prefix -> Line::getPrefixString
+                        SearchConfig.Source.Message -> Line::getMessageString
+                        SearchConfig.Source.PrefixAndMessage -> Line::getIrcLikeString
+                    }
+
+                    val sourceMatchesSearch: ((String) -> Boolean) = if (regex) {
+                        val flags = if (caseSensitive) emptySet() else setOf(RegexOption.IGNORE_CASE)
+                        text.toRegex(flags)::matches
+                    } else {
+                        if (caseSensitive) {
+                            { string: String -> string.contains(text) }
+                        } else {
+                            val locale = Locale.getDefault()
+                            val textUpper = text.toUpperCase(locale)
+                            val textLower = text.toLowerCase(locale);
+
+                            { string: String ->
+                                string.toUpperCase(locale).contains(textUpper) ||
+                                        string.toLowerCase(locale).contains(textLower)
+                            }
+                        }
+                    }
+
+                    Matcher { line -> sourceMatchesSearch(getSource(line)) }
                 }
             }
         }
+    }
+}
+
+object SearchConfig {
+    var caseSensitive = false
+    var regex = false
+    var source = Source.PrefixAndMessage
+
+    enum class Source {
+        Prefix,
+        Message,
+        PrefixAndMessage
     }
 }
