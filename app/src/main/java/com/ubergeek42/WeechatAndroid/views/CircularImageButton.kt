1f696262681f4f13c2c42abd2e479575e6cc2d00
oakkitten <oakkitten@users.noreply.github.com>
2021-03-10 18:32:39 +0000

    prevent the fab from getting stuck in invisible state in rare cases

--- app/src/main/java/com/ubergeek42/WeechatAndroid/views/CircularImageButton.kt
+++ app/src/main/java/com/ubergeek42/WeechatAndroid/views/CircularImageButton.kt
@@ -12,7 +12,6 @@ import android.view.animation.ScaleAnimation
 import androidx.appcompat.widget.AppCompatImageButton
 import com.ubergeek42.WeechatAndroid.upload.applicationContext
 import com.ubergeek42.WeechatAndroid.upload.f
-import com.ubergeek42.WeechatAndroid.utils.onAnimationEnd
 
 
 class CircularImageButton @JvmOverloads constructor(
@@ -34,9 +33,7 @@ class CircularImageButton @JvmOverloads constructor(
 
     fun hide() {
         if (visibility != INVISIBLE) {
-            val hideAnimation = makeHideAnimation().apply {
-                onAnimationEnd { visibility = INVISIBLE }
-            }
+            visibility = INVISIBLE
             startAnimation(hideAnimation)
         }
     }
@@ -57,7 +54,7 @@ class CircularImageButton @JvmOverloads constructor(
 }
 
 
-val pillOutlineProvider = object : ViewOutlineProvider() {
+private val pillOutlineProvider = object : ViewOutlineProvider() {
     override fun getOutline(view: View, outline: Outline) {
         outline.setRoundRect(0, 0, view.width, view.height, view.height.f / 2)
     }
@@ -67,14 +64,14 @@ val pillOutlineProvider = object : ViewOutlineProvider() {
 private val animationDuration = applicationContext
         .resources.getInteger(android.R.integer.config_shortAnimTime).toLong()
 
-val showAnimation = ScaleAnimation(
+private val showAnimation = ScaleAnimation(
         0f, 1f, 0f, 1f,
         Animation.RELATIVE_TO_SELF, 0.5f,
         Animation.RELATIVE_TO_SELF, 0.5f)
         .apply { duration = animationDuration }
 
-fun makeHideAnimation() = ScaleAnimation(
-        1f, 0f, 1f, 0f,
+private val hideAnimation = ScaleAnimation(
+        1f, .5f, 1f, 0.5f,
         Animation.RELATIVE_TO_SELF, 0.5f,
         Animation.RELATIVE_TO_SELF, 0.5f)
         .apply { duration = animationDuration }
