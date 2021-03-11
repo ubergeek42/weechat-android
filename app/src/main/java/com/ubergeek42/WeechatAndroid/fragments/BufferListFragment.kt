3530ae88c74d3b6fdc09c7bdfe30e2a614afdbb2
oakkitten <oakkitten@users.noreply.github.com>
2021-03-11 23:23:14 +0000

    convert BufferListFragment to Kotlin

--- app/src/main/java/com/ubergeek42/WeechatAndroid/fragments/BufferListFragment.kt
+++ app/src/main/java/com/ubergeek42/WeechatAndroid/fragments/BufferListFragment.kt
@@ -1,172 +1,157 @@
-package com.ubergeek42.WeechatAndroid.fragments;
+package com.ubergeek42.WeechatAndroid.fragments
 
-import androidx.annotation.AnyThread;
-import androidx.annotation.MainThread;
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
+import android.content.Context
+import com.ubergeek42.WeechatAndroid.relay.BufferListEye
+import com.ubergeek42.WeechatAndroid.WeechatActivity
+import com.ubergeek42.WeechatAndroid.adapters.BufferListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import android.widget.RelativeLayout
+import android.widget.EditText
+import android.widget.ImageButton
+import com.ubergeek42.cats.Cat
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.ubergeek42.WeechatAndroid.R
+import org.greenrobot.eventbus.EventBus
+import com.ubergeek42.WeechatAndroid.service.P
+import org.greenrobot.eventbus.Subscribe
+import com.ubergeek42.WeechatAndroid.service.Events.StateChangedEvent
+import com.ubergeek42.WeechatAndroid.service.RelayService
+import com.ubergeek42.WeechatAndroid.relay.BufferList
+import com.ubergeek42.WeechatAndroid.relay.Hotlist
+import android.view.View
+import androidx.annotation.AnyThread
+import androidx.annotation.MainThread
+import androidx.fragment.app.Fragment
+import com.ubergeek42.WeechatAndroid.upload.main
+import com.ubergeek42.WeechatAndroid.utils.afterTextChanged
+import com.ubergeek42.cats.Kitty
+import com.ubergeek42.cats.Root
 
-import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
+class BufferListFragment : Fragment(), BufferListEye {
+    companion object {
+        @Root private val kitty: Kitty = Kitty.make("BLF")
+    }
 
-import android.content.Context;
-import android.os.Bundle;
-import androidx.recyclerview.widget.RecyclerView;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.RelativeLayout;
+    private lateinit var weechatActivity: WeechatActivity
+    private lateinit var adapter: BufferListAdapter
 
-import com.ubergeek42.WeechatAndroid.Weechat;
-import com.ubergeek42.WeechatAndroid.adapters.BufferListAdapter;
-import com.ubergeek42.WeechatAndroid.R;
-import com.ubergeek42.WeechatAndroid.WeechatActivity;
-import com.ubergeek42.WeechatAndroid.relay.BufferList;
-import com.ubergeek42.WeechatAndroid.relay.BufferListEye;
-import com.ubergeek42.WeechatAndroid.relay.Hotlist;
-import com.ubergeek42.WeechatAndroid.service.Events;
-import com.ubergeek42.WeechatAndroid.service.P;
-import com.ubergeek42.cats.Cat;
-import com.ubergeek42.cats.Kitty;
-import com.ubergeek42.cats.Root;
-
-import static com.ubergeek42.WeechatAndroid.service.RelayService.STATE.*;
-
-
-public class BufferListFragment extends Fragment implements BufferListEye, View.OnClickListener {
-
-    final private static @Root Kitty kitty = Kitty.make("BLF");
-
-    private WeechatActivity activity;
-    private BufferListAdapter adapter;
-
-    private RecyclerView uiRecycler;
-    private RelativeLayout uiFilterBar;
-    private EditText uiFilter;
-    private ImageButton uiFilterClear;
+    private lateinit var uiRecycler: RecyclerView
+    private lateinit var uiFilterBar: RelativeLayout
+    private lateinit var uiFilter: EditText
+    private lateinit var uiFilterClear: ImageButton
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////////////////////// lifecycle
+    ////////////////////////////////////////////////////////////////////////////////////// lifecycle
     ////////////////////////////////////////////////////////////////////////////////////////////////
 
-    @MainThread @Override @Cat public void onAttach(Context context) {
-        super.onAttach(context);
-        this.activity = (WeechatActivity) context;
+    @MainThread @Cat override fun onAttach(context: Context) {
+        super.onAttach(context)
+        weechatActivity = context as WeechatActivity
     }
 
-    @MainThread @Override @Cat public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        adapter = new BufferListAdapter();
+    @MainThread @Cat override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        adapter = BufferListAdapter()
     }
 
-    @MainThread @Override @Cat public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.bufferlist, container, false);
-        uiRecycler = view.findViewById(R.id.recycler);
-        uiRecycler.setAdapter(adapter);
-        uiFilter = view.findViewById(R.id.bufferlist_filter);
-        uiFilter.addTextChangedListener(filterTextWatcher);
-        uiFilterClear = view.findViewById(R.id.bufferlist_filter_clear);
-        uiFilterClear.setOnClickListener(this);
-        uiFilterBar = view.findViewById(R.id.filter_bar);
-        return view;
+    @MainThread @Cat override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                                               savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.bufferlist, container, false)
+        uiRecycler = view.findViewById(R.id.recycler)
+        uiFilter = view.findViewById(R.id.bufferlist_filter)
+        uiFilterClear = view.findViewById(R.id.bufferlist_filter_clear)
+        uiFilterBar = view.findViewById(R.id.filter_bar)
+
+        uiRecycler.adapter = adapter
+
+        uiFilterClear.setOnClickListener {
+            uiFilter.text = null
+        }
+
+        uiFilter.afterTextChanged {
+            applyFilter()
+            adapter.onBuffersChanged()
+        }
+
+        return view
     }
 
-    @MainThread @Override @Cat public void onDestroyView() {
-        super.onDestroyView();
-        uiFilter.removeTextChangedListener(filterTextWatcher);
+    @MainThread @Cat override fun onStart() {
+        super.onStart()
+        EventBus.getDefault().register(this)
+        uiFilterBar.visibility = if (P.showBufferFilter) View.VISIBLE else View.GONE
+        applyColorSchemeToViews()
     }
 
-    @MainThread @Override @Cat public void onStart() {
-        super.onStart();
-        EventBus.getDefault().register(this);
-        uiFilterBar.setVisibility(P.showBufferFilter ? View.VISIBLE : View.GONE);
-        applyColorSchemeToViews();
-    }
-
-    @MainThread @Override @Cat public void onStop() {
-        super.onStop();
-        detachFromBufferList();
-        EventBus.getDefault().unregister(this);
-    }
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////////////////////// event
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-
-    @Subscribe(sticky=true)
-    @AnyThread @Cat public void onEvent(Events.StateChangedEvent event) {
-        if (event.state.contains(LISTED)) attachToBufferList();
-    }
-
-    //////////////////////////////////////////////////////////////////////////////////////////////// the juice
-
-    @AnyThread private void attachToBufferList() {
-        BufferList.setBufferListEye(this);
-        setFilter();
-        onBuffersChanged();
-    }
-
-    @MainThread private void detachFromBufferList() {
-        BufferList.setBufferListEye(null);
+    @MainThread @Cat override fun onStop() {
+        super.onStop()
+        detachFromBufferList()
+        EventBus.getDefault().unregister(this)
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////////////////////// BufferListEye
+    ////////////////////////////////////////////////////////////////////////////////////////// event
     ////////////////////////////////////////////////////////////////////////////////////////////////
 
-    private volatile int hotCount = 0;
+    @Subscribe(sticky = true) @AnyThread @Cat fun onEvent(event: StateChangedEvent) {
+        if (event.state.contains(RelayService.STATE.LISTED)) attachToBufferList()
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////// the juice
+
+    @AnyThread private fun attachToBufferList() {
+        BufferList.setBufferListEye(this)
+        applyFilter()
+        onBuffersChanged()
+    }
+
+    @MainThread private fun detachFromBufferList() {
+        BufferList.setBufferListEye(null)
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////// BufferListEye
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Volatile private var hotCount = 0
 
     // if hot count has changed and > 0, scroll to top. note that if the scroll operation is not
     // posted, it cat try to scroll to the view that was at position 0 before the diff update
-    @AnyThread @Override @Cat public void onBuffersChanged() {
-        adapter.onBuffersChanged();
-        int hotCount = Hotlist.getHotCount();
-        Weechat.runOnMainThread(() -> {
-            if (this.hotCount != (this.hotCount = hotCount) && hotCount > 0)
-                Weechat.runOnMainThread(() -> uiRecycler.smoothScrollToPosition(0));
-            activity.updateHotCount(hotCount);
-            activity.onBuffersChanged();
-        });
-    }
+    // todo don't update on every change?
+    // todo move hotlist updates to the activity
+    @AnyThread @Cat override fun onBuffersChanged() {
+        adapter.onBuffersChanged()
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////////////////////// other
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-
-    private TextWatcher filterTextWatcher = new TextWatcher() {
-        @MainThread @Override public void afterTextChanged(Editable a) {}
-        @MainThread @Override public void beforeTextChanged(CharSequence arg0, int a, int b, int c) {}
-
-        @MainThread @Override public void onTextChanged(CharSequence s, int start, int before, int count) {
-            if (adapter != null) {
-                setFilter();
-                adapter.onBuffersChanged();
+        val hotCount = Hotlist.getHotCount()
+        main {
+            if (this.hotCount != hotCount) {
+                this.hotCount = hotCount
+                if (hotCount > 0) uiRecycler.smoothScrollToPosition(0)
             }
+            weechatActivity.updateHotCount(hotCount)
+            weechatActivity.onBuffersChanged()
         }
-    };
-
-
-    @AnyThread private void setFilter() {
-        String text = uiFilter.getText().toString();
-        adapter.setFilter(text, true);
-        Weechat.runOnMainThread(() -> uiFilterClear.setVisibility((text.length() == 0) ?
-                View.INVISIBLE : View.VISIBLE));
     }
 
-    // the only button we've got: clear text in the filter
-    @MainThread @Override public void onClick(View v) {
-        uiFilter.setText(null);
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////// other
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @AnyThread private fun applyFilter() {
+        val text = uiFilter.text.toString()
+        adapter.setFilter(text, true)
+        main {
+            uiFilterClear.visibility = if (text.isEmpty()) View.INVISIBLE else View.VISIBLE
+        }
     }
 
-    @Override public @NonNull String toString() {
-        return "BL";
+
+    private fun applyColorSchemeToViews() {
+        uiFilterBar.setBackgroundColor(P.colorPrimary)
+        uiRecycler.setBackgroundColor(P.colorPrimary)
     }
 
-    private void applyColorSchemeToViews() {
-        uiFilterBar.setBackgroundColor(P.colorPrimary);
-        uiRecycler.setBackgroundColor(P.colorPrimary);
-    }
+    override fun toString() = "BL"
 }
