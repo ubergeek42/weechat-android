20297327e1621031947c39f5bba07f4846b1d917
oakkitten <oakkitten@users.noreply.github.com>
2021-03-11 01:59:54 +0000

    convert Copy to Kotlin

--- app/src/main/java/com/ubergeek42/WeechatAndroid/copypaste/Copy.kt
+++ app/src/main/java/com/ubergeek42/WeechatAndroid/copypaste/Copy.kt
@@ -1,60 +1,55 @@
-package com.ubergeek42.WeechatAndroid.copypaste;
+package com.ubergeek42.WeechatAndroid.copypaste
 
-import android.content.Context;
-import android.text.ClipboardManager;
-import android.text.TextUtils;
-import android.text.style.URLSpan;
-import android.view.LayoutInflater;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.content.Context
+import android.text.ClipboardManager
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ubergeek42.WeechatAndroid.R
+import com.ubergeek42.WeechatAndroid.dialogs.FancyAlertDialogBuilder
+import com.ubergeek42.WeechatAndroid.relay.Line
+import com.ubergeek42.WeechatAndroid.upload.applicationContext
+import com.ubergeek42.WeechatAndroid.views.LineView
+import java.util.*
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.recyclerview.widget.RecyclerView;
+object Copy {
+    @JvmStatic fun showCopyDialog(lineView: LineView): Boolean {
+        lineView.showCopyDialog()
+        return true
+    }
+}
 
-import com.ubergeek42.WeechatAndroid.R;
-import com.ubergeek42.WeechatAndroid.relay.Line;
-import com.ubergeek42.WeechatAndroid.dialogs.FancyAlertDialogBuilder;
-import com.ubergeek42.WeechatAndroid.views.LineView;
+fun LineView.showCopyDialog() {
+    val context = context
+    val line = tag as Line
 
-import java.util.ArrayList;
+    val list = ArrayList<CharSequence>()
+    if (line.prefixString.isNotEmpty()) list.add(line.ircLikeString)
+    list.add(line.messageString)
+    list.addAll(urls.map { it.url })
 
-public class Copy {
-    public static boolean showCopyDialog(LineView lineView) {
-        Context context = lineView.getContext();
-        Line line = (Line) lineView.getTag();
-        ArrayList<CharSequence> list = new ArrayList<>();
+    val dialog = FancyAlertDialogBuilder(context).create()
 
-        if (!TextUtils.isEmpty(line.getPrefixString()))
-            list.add(line.getIrcLikeString());
-        list.add(line.getMessageString());
+    val layout = LayoutInflater.from(context).inflate(R.layout.dialog_copy, null) as ViewGroup
+    val recyclerView = layout.findViewById<RecyclerView>(R.id.list)
+    val title = layout.findViewById<TextView>(R.id.title)
 
-        for (URLSpan urlSpan : lineView.getUrls()) {
-            String url = urlSpan.getURL();
-            if (!list.contains(url)) list.add(url);
-        }
+    title.setText(R.string.dialog__copy__title)
 
-        AlertDialog dialog = new FancyAlertDialogBuilder(context).create();
-
-        ViewGroup layout = (ViewGroup) LayoutInflater.from(context)
-                .inflate(R.layout.dialog_copy, null);
-
-        RecyclerView recyclerView = layout.findViewById(R.id.list);
-        TextView title = layout.findViewById(R.id.title);
-
-        title.setText(R.string.dialog__copy__title);
-
-        recyclerView.setAdapter(new CopyAdapter(context, list, item -> {
-            setClipboard(context, item);
-            dialog.dismiss();
-        }));
-
-        dialog.setView(layout);
-        dialog.show();
-        return true;
+    recyclerView.adapter = CopyAdapter(context, list.distinct()) { item ->
+        setClipboard(item)
+        dialog.dismiss()
     }
 
-    private static void setClipboard(Context context, CharSequence text) {
-        ClipboardManager cm = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
-        if (cm != null) cm.setText(text);
-    }
+    dialog.setView(layout)
+    dialog.show()
+}
+
+
+private fun setClipboard(text: CharSequence) {
+    val clipboardManager = applicationContext
+            .getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager?
+    clipboardManager?.text = text
 }
