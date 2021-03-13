85c88d41e9fe2a9e510c025a5339122c927430e7
oakkitten <oakkitten@users.noreply.github.com>
2021-03-13 00:52:48 +0000

    deal with the keyboard state changes via inset listener

--- app/src/main/java/com/ubergeek42/WeechatAndroid/utils/ToolbarController.java
+++ app/src/main/java/com/ubergeek42/WeechatAndroid/utils/ToolbarController.java
@@ -1,24 +1,24 @@
 package com.ubergeek42.WeechatAndroid.utils;
 
-import androidx.appcompat.widget.Toolbar;
-import androidx.lifecycle.Lifecycle;
-
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.DecelerateInterpolator;
 
 import com.ubergeek42.WeechatAndroid.R;
 import com.ubergeek42.WeechatAndroid.WeechatActivity;
 import com.ubergeek42.WeechatAndroid.service.P;
+import com.ubergeek42.WeechatAndroid.views.SystemAreaHeightExaminer;
+import com.ubergeek42.WeechatAndroid.views.SystemAreaHeightObserver;
 
-public class ToolbarController implements ViewTreeObserver.OnGlobalLayoutListener {
+
+public class ToolbarController implements SystemAreaHeightObserver {
     private final WeechatActivity activity;
     private final View toolbarContainer;
-    private final View root;
     private final View mainView;
     private final int toolbarHeight;
+
+    private final SystemAreaHeightExaminer systemAreaHeightExaminer;
 
     private boolean shown = true;
     public boolean keyboardVisible = false;
@@ -27,13 +27,15 @@ public class ToolbarController implements ViewTreeObserver.OnGlobalLayoutListene
         this.activity = activity;
         this.toolbarContainer = activity.findViewById(R.id.toolbar_container);
         this.mainView = activity.findViewById(R.id.main_viewpager);
-        this.root = activity.findViewById(android.R.id.content);
-        root.getViewTreeObserver().addOnGlobalLayoutListener(this);
         toolbarHeight = toolbarContainer.getLayoutParams().height;
+
+        systemAreaHeightExaminer = SystemAreaHeightExaminer.obtain();
+        systemAreaHeightExaminer.setObserver(this);
+        systemAreaHeightExaminer.onActivityCreated(activity);
     }
 
     public void detach() {
-        root.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+        systemAreaHeightExaminer.onActivityDestroyed(activity);
     }
 
     private int _dy = 0;
@@ -93,24 +95,8 @@ public class ToolbarController implements ViewTreeObserver.OnGlobalLayoutListene
 
     private int initialSystemAreaHeight = -1;
 
-    // windowHeight is the height of the activity that includes the height of the status bar and the
-    // navigation bar. if the activity is split, this height seems to be only including the system
-    // bar that the activity is “touching”. this height doesn't include the keyboard height per se,
-    // but if the activity changes size due to the keyboard, this number remains the same.
-    // activityHeight is the height of the activity not including any of the system stuff.
-    @Override public void onGlobalLayout() {
+    @Override public void onSystemAreaHeightChanged(int systemAreaHeight) {
         if (canNotAutoHide()) return;
-
-        // on android 7, if changing the day/night theme in settings, the activity can be recreated
-        // right away but with a wrong window height. so we wait until it's actually resumed
-        if (activity.getLifecycle().getCurrentState() != Lifecycle.State.RESUMED) return;
-
-        int windowHeight = root.getRootView().getHeight();
-        int activityHeight = root.getHeight();
-        int systemAreaHeight = windowHeight - activityHeight;
-
-        if (windowHeight <= 0) return;
-        if (systemAreaHeight <= 0) return;
 
         // note the initial system area (assuming keyboard closed) and return. we should be getting
         // a few more calls to this method without any changes to the height numbers
@@ -122,9 +108,46 @@ public class ToolbarController implements ViewTreeObserver.OnGlobalLayoutListene
         // weed out some insanity that's happening when the window is in split screen mode. it seems
         // that while resizing some elements can temporarily have the height 0.
         if (systemAreaHeight < initialSystemAreaHeight) return;
-        if (activityHeight == 0) return;
 
         boolean keyboardVisible = systemAreaHeight - initialSystemAreaHeight > 20;
         onSoftwareKeyboardStateChanged(keyboardVisible);
     }
 }
+
+//    // windowHeight is the height of the activity that includes the height of the status bar and the
+//    // navigation bar. if the activity is split, this height seems to be only including the system
+//    // bar that the activity is “touching”. this height doesn't include the keyboard height per se,
+//    // but if the activity changes size due to the keyboard, this number remains the same.
+//    // activityHeight is the height of the activity not including any of the system stuff.
+//    @Override public void onGlobalLayout() {
+//        if (canNotAutoHide()) return;
+//
+//        // on android 7, if changing the day/night theme in settings, the activity can be recreated
+//        // right away but with a wrong window height. so we wait until it's actually resumed
+//        if (activity.getLifecycle().getCurrentState() != Lifecycle.State.RESUMED) return;
+//
+//        int windowHeight = root.getRootView().getHeight();
+//        int activityHeight = root.getHeight();
+//        int systemAreaHeight = windowHeight - activityHeight;
+//
+//        System.out.println("windowHeight " + windowHeight + " activityHeight " + activityHeight + " systemAreaHeight " + systemAreaHeight);
+//
+//
+//        if (windowHeight <= 0) return;
+//        if (systemAreaHeight <= 0) return;
+//
+//        // note the initial system area (assuming keyboard closed) and return. we should be getting
+//        // a few more calls to this method without any changes to the height numbers
+//        if (initialSystemAreaHeight == -1) {
+//            initialSystemAreaHeight = systemAreaHeight;
+//            return;
+//        }
+//
+//        // weed out some insanity that's happening when the window is in split screen mode. it seems
+//        // that while resizing some elements can temporarily have the height 0.
+//        if (systemAreaHeight < initialSystemAreaHeight) return;
+//        if (activityHeight == 0) return;
+//
+//        boolean keyboardVisible = systemAreaHeight - initialSystemAreaHeight > 20;
+//        onSoftwareKeyboardStateChanged(keyboardVisible);
+//    }
--- app/src/main/java/com/ubergeek42/WeechatAndroid/views/FullScreenActivityController.kt
+++ app/src/main/java/com/ubergeek42/WeechatAndroid/views/FullScreenActivityController.kt
@@ -3,9 +3,12 @@ package com.ubergeek42.WeechatAndroid.views
 import android.os.Build
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.updateLayoutParams
 import androidx.core.view.updatePadding
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.recyclerview.widget.RecyclerView
 import com.ubergeek42.WeechatAndroid.R
 import com.ubergeek42.WeechatAndroid.WeechatActivity
@@ -14,6 +17,20 @@ import com.ubergeek42.WeechatAndroid.utils.WeaselMeasuringViewPager
 
 
 val FULL_SCREEN_DRAWER_ENABLED = Build.VERSION.SDK_INT >= 27
+
+
+object SystemWindowInsets {
+    var top = 0
+    var bottom = 0
+}
+
+
+fun interface InsetListener {
+    fun onInsetsChanged()
+}
+
+
+private val insetListeners = mutableListOf<InsetListener>()
 
 
 fun onWeechatActivityCreated(activity: WeechatActivity) {
@@ -34,17 +51,23 @@ fun onWeechatActivityCreated(activity: WeechatActivity) {
             View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 
     rootView.setOnApplyWindowInsetsListener listener@{ _, insets ->
-        val systemTop = insets.systemWindowInsetTop
-        val systemBottom = insets.systemWindowInsetBottom
+        SystemWindowInsets.top = insets.systemWindowInsetTop
+        SystemWindowInsets.bottom = insets.systemWindowInsetBottom
 
-        toolbarContainer.updatePadding(top = systemTop)
-        navigationPadding.updateLayoutParams<ViewGroup.MarginLayoutParams> { height = systemBottom }
-        viewPager.updateLayoutParams<ViewGroup.MarginLayoutParams> { bottomMargin = systemBottom }
-
-        // todo deal with the keyboard
+        insetListeners.forEach { it.onInsetsChanged() }
 
         insets
     }
+
+    val weechatActivityInsetsListener = InsetListener {
+        toolbarContainer.updatePadding(top = SystemWindowInsets.top)
+        navigationPadding.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+            height = SystemWindowInsets.bottom }
+        viewPager.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+            bottomMargin = SystemWindowInsets.bottom }
+    }
+
+    insetListeners.add(weechatActivityInsetsListener)
 }
 
 
@@ -74,20 +97,84 @@ fun onBufferListFragmentStarted(bufferList: Fragment) {
         }
     }
 
-    bufferListView.setOnApplyWindowInsetsListener { _, insets ->
-        SystemWindowInsets.top = insets.systemWindowInsetTop
-        SystemWindowInsets.bottom = insets.systemWindowInsetBottom
-
-        applyInsets()
-
-        insets
-    }
+    insetListeners.add(InsetListener { applyInsets() })
 
     applyInsets()
 }
 
 
-object SystemWindowInsets {
-    var top = 0
-    var bottom = 0
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+fun interface SystemAreaHeightObserver {
+    fun onSystemAreaHeightChanged(size: Int)
+}
+
+
+abstract class SystemAreaHeightExaminer {
+    var observer: SystemAreaHeightObserver? = null
+
+    abstract fun onActivityCreated(activity: WeechatActivity)
+    abstract fun onActivityDestroyed(activity: WeechatActivity)
+
+    companion object {
+        @JvmStatic fun obtain() = if (FULL_SCREEN_DRAWER_ENABLED)
+            NewSystemAreaHeightExaminer() else OldSystemAreaHeightExaminer()
+    }
+}
+
+
+class OldSystemAreaHeightExaminer : SystemAreaHeightExaminer() {
+    private lateinit var activity: AppCompatActivity
+    private lateinit var content: View
+    private lateinit var rootView: View
+
+    override fun onActivityCreated(activity: WeechatActivity) {
+        this.activity = activity
+        content = activity.findViewById(android.R.id.content)
+        rootView = content.rootView
+        content.viewTreeObserver.addOnGlobalLayoutListener(globalLayoutListener)
+    }
+
+    override fun onActivityDestroyed(activity: WeechatActivity) {
+        content.viewTreeObserver.addOnGlobalLayoutListener(globalLayoutListener)
+    }
+
+    // windowHeight is the height of the activity that includes the height of the status bar and the
+    // navigation bar. if the activity is split, this height seems to be only including the system
+    // bar that the activity is “touching”. this height doesn't include the keyboard height per se,
+    // but if the activity changes size due to the keyboard, this number remains the same.
+    // activityHeight is the height of the activity not including any of the system stuff.
+    private val globalLayoutListener = ViewTreeObserver.OnGlobalLayoutListener listener@{
+        // on android 7, if changing the day/night theme in settings, the activity can be recreated
+        // right away but with a wrong window height. so we wait until it's actually resumed
+        if (activity.lifecycle.currentState != Lifecycle.State.RESUMED) return@listener
+
+        val windowHeight = rootView.height
+        val activityHeight = content.height
+        val systemAreaHeight = windowHeight - activityHeight
+
+        // weed out some insanity that's happening when the window is in split screen mode.
+        // it seems that while resizing some elements can temporarily have the height 0.
+        if (windowHeight <= 0 || activityHeight <= 0 || systemAreaHeight <= 0) return@listener
+
+        observer?.onSystemAreaHeightChanged(systemAreaHeight)
+    }
+}
+
+
+private class NewSystemAreaHeightExaminer : SystemAreaHeightExaminer() {
+    override fun onActivityCreated(activity: WeechatActivity) {
+        insetListeners.add(insetListener)
+    }
+
+    override fun onActivityDestroyed(activity: WeechatActivity) {
+        insetListeners.remove(insetListener)
+    }
+
+    private val insetListener = InsetListener {
+        observer?.onSystemAreaHeightChanged(SystemWindowInsets.bottom)
+    }
 }
