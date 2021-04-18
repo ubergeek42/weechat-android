d3c28657c73ab0919f462095fb174b5a7674f675
oakkitten <oakkitten@users.noreply.github.com>
2021-04-18 18:23:27 +0100

    refactor ThemeManager & ThemePreference

--- app/src/main/java/androidx/preference/ThemeManager.kt
+++ app/src/main/java/androidx/preference/ThemeManager.kt
@@ -1,84 +1,73 @@
 package androidx.preference
 
 import android.content.Context
-import android.content.res.AssetManager
-import android.preference.PreferenceManager
-import com.ubergeek42.WeechatAndroid.R
 import com.ubergeek42.WeechatAndroid.service.P
+import com.ubergeek42.WeechatAndroid.upload.applicationContext
+import com.ubergeek42.WeechatAndroid.upload.suppress
 import com.ubergeek42.WeechatAndroid.utils.Constants
-import com.ubergeek42.WeechatAndroid.utils.Toaster
-import com.ubergeek42.cats.Kitty
-import com.ubergeek42.cats.Root
 import com.ubergeek42.weechat.ColorScheme
 import java.io.File
 import java.io.FileInputStream
-import java.io.IOException
 import java.util.*
 
+
 object ThemeManager {
-    @Root
-    private val kitty: Kitty = Kitty.make()
     fun getThemeSearchDirectories(context: Context): List<String> {
-        val out: MutableList<String> = ArrayList()
-        val appSpecificFontFolder = context.getExternalFilesDir(CUSTOM_THEMES_DIRECTORY)
-        if (appSpecificFontFolder != null) out.add(appSpecificFontFolder.toString())
-        return out
+        val internalFontFolder = context.getExternalFilesDir(CUSTOM_THEMES_DIRECTORY)
+        return if (internalFontFolder != null) listOf(internalFontFolder.toString()) else listOf()
     }
 
     @JvmStatic fun loadColorSchemeFromPreferences(context: Context) {
         val path = PreferenceManager.getDefaultSharedPreferences(context)
                 .getString(if (P.darkThemeActive) Constants.PREF_COLOR_SCHEME_NIGHT else Constants.PREF_COLOR_SCHEME_DAY,
-                        if (P.darkThemeActive) Constants.PREF_COLOR_SCHEME_NIGHT_D else Constants.PREF_COLOR_SCHEME_DAY_D)
-        val p = loadColorScheme(path, context.assets)
-        if (p == null) Toaster.ErrorToast.show(R.string.pref__ThemeManager__error_loading_color_scheme, path!!) else ColorScheme.set(ColorScheme(p))
+                           if (P.darkThemeActive) Constants.PREF_COLOR_SCHEME_NIGHT_D else Constants.PREF_COLOR_SCHEME_DAY_D)
+        suppress<Exception>(showToast = true) {
+            val themeInfo = ThemeInfo.fromPath(path!!)
+            ColorScheme.set(ColorScheme(themeInfo.properties))
+        }
     }
 
-    @JvmStatic fun enumerateThemes(context: Context): LinkedList<ThemeInfo> {
-        val themes = LinkedList<ThemeInfo>()
-        val manager = context.assets
+    private fun Context.getAssetThemes() = sequence {
+        assets.list("")?.forEach { assetName ->
+            if (assetName.endsWith("theme.properties", true)) {
+                yield(ThemeInfo.fromPath(assetName))
+            }
+        }
+    }
 
-        // load themes from assets
-        try {
-            val builtin_themes = manager.list("")
-            if (builtin_themes != null) {
-                for (theme in builtin_themes) {
-                    if (!theme.toLowerCase().endsWith("theme.properties")) continue
-                    val p = loadColorScheme(theme, manager)
-                    if (p != null) themes.add(ThemeInfo(p.getProperty("name", theme), theme))
+    private fun Context.getExternalThemes() = sequence {
+        getThemeSearchDirectories(this@getExternalThemes).forEach { directoryName ->
+            val directory = File(directoryName)
+            if (directory.exists()) {
+                directory.listFiles()?.forEach { file ->
+                    suppress<Exception>(showToast = true) {
+                        yield(ThemeInfo.fromPath(file.absolutePath))
+                    }
                 }
             }
-        } catch (e: IOException) {
-            e.printStackTrace()
         }
+    }
 
-        // load themes from disk
-        for (directory in getThemeSearchDirectories(context)) {
-            val dir = File(directory)
-            if (!dir.exists()) continue
-            val files = dir.listFiles() ?: continue
-            for (file in files) {
-                if (!file.name.toLowerCase().endsWith("theme.properties")) continue
-                val p = loadColorScheme(file.absolutePath, null)
-                if (p != null) themes.add(ThemeInfo(p.getProperty("name", file.name), file.absolutePath))
+    fun enumerateThemes(context: Context) = context.getAssetThemes() + context.getExternalThemes()
+}
+
+
+class ThemeInfo(
+    val name: String,
+    val path: String,
+    val properties: Properties,
+) {
+    companion object {
+        fun fromPath(path: String): ThemeInfo {
+            val properties = Properties()
+            val name = if (path.startsWith("/")) {
+                properties.load(FileInputStream(path))
+                properties.getProperty("name") ?: File(path).name
+            } else {
+                properties.load(applicationContext.assets.open(path))
+                properties.getProperty("name") ?: path
             }
-        }
-        return themes
-    }
-
-    private fun loadColorScheme(path: String?, manager: AssetManager?): Properties? {
-        val p = Properties()
-        return try {
-            p.load(if (path!!.startsWith("/")) FileInputStream(path) else manager!!.open(path))
-            p
-        } catch (e: IOException) {
-            kitty.error("Failed to load file $path", e)
-            null
-        }
-    }
-
-    class ThemeInfo internal constructor(var name: String, var path: String) : Comparable<ThemeInfo> {
-        override fun compareTo(another: ThemeInfo): Int {
-            return name.compareTo(another.name)
+            return ThemeInfo(name, path, properties)
         }
     }
 }
--- app/src/main/java/androidx/preference/ThemePreference.kt
+++ app/src/main/java/androidx/preference/ThemePreference.kt
@@ -3,39 +3,37 @@ package androidx.preference
 import android.content.Context
 import android.content.DialogInterface
 import android.content.res.TypedArray
-import android.text.TextUtils
 import android.util.AttributeSet
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
-import androidx.preference.ThemeManager.ThemeInfo
 import androidx.preference.ThemeManager.enumerateThemes
 import androidx.preference.ThemePreferenceHelp.Companion.getThemeName
 import com.ubergeek42.WeechatAndroid.R
 import com.ubergeek42.WeechatAndroid.utils.Toaster
-import java.util.*
+
 
 class ThemePreference(context: Context?, attrs: AttributeSet?) : DialogPreference(context, attrs), DialogFragmentGetter {
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    private var defaultValue: String? = null
-    private val themePath: String?
-        private get() = sharedPreferences.getString(key, defaultValue)
-
-    private fun setThemePath(path: String) {
-        sharedPreferences.edit().putString(key, path).apply()
-        notifyChanged()
-    }
+    private lateinit var defaultValue: String
 
     override fun onGetDefaultValue(a: TypedArray, index: Int): Any {
-        return a.getString(index).also { defaultValue = it }!!
+        defaultValue = a.getString(index) ?: ""
+        return defaultValue
     }
+
+    private var themePath: String
+        get() = sharedPreferences.getString(key, defaultValue) ?: ""
+        set(path) {
+            sharedPreferences.edit().putString(key, path).apply()
+            notifyChanged()
+        }
 
     override fun getSummary(): CharSequence {
         val path = themePath
-        return if (TextUtils.isEmpty(path)) {
+        return if (path.isEmpty()) {
             context.getString(R.string.pref__ThemePreference__not_set)
         } else {
             try {
-                getThemeName(context, path!!)
+                getThemeName(context, path)
             } catch (e: Exception) {
                 Toaster.ErrorToast.show(e)
                 "Error"
@@ -48,30 +46,26 @@ class ThemePreference(context: Context?, attrs: AttributeSet?) : DialogPreferenc
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
+
     class ThemePreferenceFragment : PreferenceDialogFragmentCompat(), DialogInterface.OnClickListener {
-        private var themes: LinkedList<ThemeInfo>? = null
+        private lateinit var themes: List<ThemeInfo>
+
+        @OptIn(ExperimentalStdlibApi::class)
         override fun onPrepareDialogBuilder(builder: AlertDialog.Builder) {
             super.onPrepareDialogBuilder(builder)
-            themes = enumerateThemes(requireContext())
-            Collections.sort(themes)
 
-            // find index of the current theme, and while we are at it
-            // create a CharSequence[] copy of the theme name list
-            val list = arrayOfNulls<CharSequence>(themes!!.size)
+            themes = enumerateThemes(requireContext()).sortedBy { it.name.lowercase() }.toList()
+
+            val themeNames = themes.map { it.name }.toTypedArray()
             val currentPath = (preference as ThemePreference).themePath
-            var idx = 0
-            var checked_item = 0
-            for (theme in themes!!) {
-                if (theme.path == currentPath) checked_item = idx
-                list[idx] = theme.name
-                idx++
-            }
-            builder.setSingleChoiceItems(list, checked_item, this)
+            val currentIndex = themes.indexOfFirst { it.path == currentPath }   // -1 is ok
+
+            builder.setSingleChoiceItems(themeNames, currentIndex, this)
             builder.setPositiveButton(null, null)
         }
 
         override fun onClick(dialog: DialogInterface, which: Int) {
-            if (which >= 0) (preference as ThemePreference).setThemePath(themes!![which].path)
+            if (which >= 0) (preference as ThemePreference).themePath = (themes[which].path)
             dialog.dismiss()
         }
 
