444d3d7f04fb60fbbf5a9aa0ce4c8ffa850adaf8
oakkitten <oakkitten@users.noreply.github.com>
2021-04-18 21:27:53 +0100

    convert MigratePreferences to Kotlin; add migration for fonts & themes
    it only alerts the user that they may have to import fonts or themes

--- app/src/main/java/com/ubergeek42/WeechatAndroid/utils/MigratePreferences.kt
+++ app/src/main/java/com/ubergeek42/WeechatAndroid/utils/MigratePreferences.kt
@@ -1,184 +1,196 @@
-package com.ubergeek42.WeechatAndroid.utils;
+package com.ubergeek42.WeechatAndroid.utils
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
+import android.content.Context
+import androidx.preference.PreferenceManager
+import androidx.preference.PrivateKeyPickerPreference
+import com.ubergeek42.WeechatAndroid.upload.applicationContext
+import com.ubergeek42.WeechatAndroid.utils.AndroidKeyStoreUtils.InsideSecurityHardware
+import com.ubergeek42.cats.Kitty
+import com.ubergeek42.cats.Root
+import com.ubergeek42.weechat.relay.connection.SSHConnection
+import java.util.*
 
-import androidx.preference.PrivateKeyPickerPreference;
 
-import com.ubergeek42.cats.Kitty;
-import com.ubergeek42.cats.Root;
-import com.ubergeek42.weechat.relay.connection.SSHConnection;
-
-import java.security.KeyPair;
-import java.util.ArrayList;
-import java.util.List;
-
-import static androidx.preference.PrivateKeyPickerPreference.STORED_IN_KEYSTORE;
-import static com.ubergeek42.WeechatAndroid.utils.Constants.PREF_SSH_AUTHENTICATION_METHOD;
-import static com.ubergeek42.WeechatAndroid.utils.Constants.PREF_SSH_AUTHENTICATION_METHOD_KEY;
-import static com.ubergeek42.WeechatAndroid.utils.Constants.PREF_SSH_AUTHENTICATION_METHOD_PASSWORD;
-import static com.ubergeek42.WeechatAndroid.utils.Constants.PREF_SSH_KEY_FILE;
-import static com.ubergeek42.WeechatAndroid.utils.Constants.PREF_SSH_KEY_FILE_D;
-import static com.ubergeek42.WeechatAndroid.utils.Constants.PREF_SSH_PASSWORD;
-import static com.ubergeek42.WeechatAndroid.utils.AndroidKeyStoreUtils.isInsideSecurityHardware;
-import static com.ubergeek42.WeechatAndroid.utils.AndroidKeyStoreUtils.putKeyPairIntoAndroidKeyStore;
-import static com.ubergeek42.WeechatAndroid.utils.AndroidKeyStoreUtils.InsideSecurityHardware;
-import static com.ubergeek42.WeechatAndroid.utils.Toaster.InfoToast;
-
-public class MigratePreferences {
-    final private static @Root Kitty kitty = Kitty.make();
-
-    final static String VERSION_KEY = "preferences-version-key";
-
-    final SharedPreferences preferences;
-
-    List<Migrator> migrators = new ArrayList<>();
-
-    public MigratePreferences(Context context) {
-        preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        initMigrators();
+class MigratePreferences(val context: Context) {
+    companion object {
+        @Root private val kitty = Kitty.make() as Kitty
     }
 
-    private class Migrator {
-        public int oldVersion;
-        public int newVersion;
-        public Runnable runnable;
+    private val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+    private var migrators = mutableListOf<Migrator>()
 
-        public Migrator(int oldVersion, int newVersion, Runnable runnable) {
-            this.oldVersion = oldVersion;
-            this.newVersion = newVersion;
-            this.runnable = runnable;
-        }
-
-        void migrate() {
-            kitty.info("Migrating preferences from version %s to %s", oldVersion, newVersion);
-            if (oldVersion != preferences.getInt(VERSION_KEY, 0))
-                throw new RuntimeException("Could not migrate");
-            runnable.run();
-            preferences.edit().putInt(VERSION_KEY, newVersion).apply();
+    private inner class Migrator(
+        val oldVersion: Int,
+        val newVersion: Int,
+        val action: Migrator.() -> Unit
+    ) {
+        fun migrate() {
+            kitty.info("Migrating preferences from version %s to %s", oldVersion, newVersion)
+            if (oldVersion != preferences.getInt(VERSION_KEY, 0)) throw RuntimeException("Could not migrate")
+            this.action()
+            preferences.edit().putInt(VERSION_KEY, newVersion).apply()
         }
     }
 
+    fun migrate() {
+        kitty.info("Preferences version: %s", preferences.getInt(VERSION_KEY, 0))
 
-    public void migrate() {
-        kitty.info("Preferences version: %s", preferences.getInt(VERSION_KEY, 0));
-        main: while (true) {
-            int version = preferences.getInt(VERSION_KEY, 0);
-            for (Migrator migrator : migrators) {
+        outer@ while (true) {
+            val version = preferences.getInt(VERSION_KEY, 0)
+            for (migrator in migrators) {
                 if (migrator.oldVersion == version) {
-                    migrator.migrate();
-                    continue main;
+                    migrator.migrate()
+                    continue@outer
                 }
             }
-            return;
+            return
         }
     }
 
-    void initMigrators() {
-        migrators.add(new Migrator(0, 1, () -> {
-            if (!preferences.contains(Constants.Deprecated.PREF_SSH_PASS))
-                return;
-            String sshPass = preferences.getString(Constants.Deprecated.PREF_SSH_PASS,
-                    Constants.Deprecated.PREF_SSH_PASS_D);
-            String sshKey = preferences.getString(Constants.Deprecated.PREF_SSH_KEY,
-                    Constants.Deprecated.PREF_SSH_KEY_D);
+    init {
+        fun add(oldVersion: Int, newVersion: Int, action: Migrator.() -> Unit) {
+            migrators.add(Migrator(oldVersion, newVersion, action))
+        }
 
-            String authenticationMethod, sshPassword, sshKeyFile, sshPassphrase;
+        add(0, 1) {
+            if (!preferences.contains(Constants.Deprecated.PREF_SSH_PASS)) return@add
+
+            val sshPass = preferences.getString(Constants.Deprecated.PREF_SSH_PASS,
+                                                Constants.Deprecated.PREF_SSH_PASS_D)
+            val sshKey = preferences.getString(Constants.Deprecated.PREF_SSH_KEY,
+                                               Constants.Deprecated.PREF_SSH_KEY_D)
+            val authenticationMethod: String
+            val sshPassword: String?
+            val sshKeyFile: String?
+            val sshPassphrase: String?
+
             if (sshKey != null) {
-                authenticationMethod = PREF_SSH_AUTHENTICATION_METHOD_KEY;
-                sshPassword = null;
-                sshPassphrase = sshPass;
-                sshKeyFile = sshKey;
+                authenticationMethod = Constants.PREF_SSH_AUTHENTICATION_METHOD_KEY
+                sshPassword = null
+                sshPassphrase = sshPass
+                sshKeyFile = sshKey
             } else {
-                authenticationMethod = PREF_SSH_AUTHENTICATION_METHOD_PASSWORD;
-                sshPassword = sshPass;
-                sshPassphrase = null;
-                sshKeyFile = null;
+                authenticationMethod = Constants.PREF_SSH_AUTHENTICATION_METHOD_PASSWORD
+                sshPassword = sshPass
+                sshPassphrase = null
+                sshKeyFile = null
             }
 
             preferences.edit()
-                    .putString(PREF_SSH_AUTHENTICATION_METHOD, authenticationMethod)
-                    .putString(PREF_SSH_PASSWORD, sshPassword)
-                    .putString(PREF_SSH_KEY_FILE, sshKeyFile)
+                    .putString(Constants.PREF_SSH_AUTHENTICATION_METHOD, authenticationMethod)
+                    .putString(Constants.PREF_SSH_PASSWORD, sshPassword)
+                    .putString(Constants.PREF_SSH_KEY_FILE, sshKeyFile)
                     .putString(Constants.Deprecated.PREF_SSH_KEY_PASSPHRASE, sshPassphrase)
-
                     .remove(Constants.Deprecated.PREF_SSH_PASS)
                     .remove(Constants.Deprecated.PREF_SSH_KEY)
-                    .apply();
-        }));
+                    .apply()
+        }
 
-        migrators.add(new Migrator(1, 2, () -> {
-            String sshKeyFile = preferences.getString(PREF_SSH_KEY_FILE, PREF_SSH_KEY_FILE_D);
-            String sshPassphrase = preferences.getString(Constants.Deprecated.PREF_SSH_KEY_PASSPHRASE,
-                    Constants.Deprecated.PREF_SSH_KEY_PASSPHRASE_D);
-            if (sshKeyFile == null)
-                return;
+        add(1, 2) {
+            val sshKeyFile = preferences.getString(Constants.PREF_SSH_KEY_FILE,
+                                                   Constants.PREF_SSH_KEY_FILE_D) ?: return@add
 
-            if (!STORED_IN_KEYSTORE.equals(sshKeyFile)) {
-                byte[] sshKeyFileBytes = PrivateKeyPickerPreference.getData(sshKeyFile);
+            val sshPassphrase = preferences.getString(Constants.Deprecated.PREF_SSH_KEY_PASSPHRASE,
+                                                      Constants.Deprecated.PREF_SSH_KEY_PASSPHRASE_D)
+
+            if (PrivateKeyPickerPreference.STORED_IN_KEYSTORE != sshKeyFile) {
+                val sshKeyFileBytes = PrivateKeyPickerPreference.getData(sshKeyFile)
                 try {
-                    KeyPair keyPair = SSHConnection.makeKeyPair(sshKeyFileBytes, sshPassphrase);
-                    putKeyPairIntoAndroidKeyStore(keyPair, SSHConnection.KEYSTORE_ALIAS);
+                    val keyPair = SSHConnection.makeKeyPair(sshKeyFileBytes, sshPassphrase)
+                    AndroidKeyStoreUtils.putKeyPairIntoAndroidKeyStore(keyPair, SSHConnection.KEYSTORE_ALIAS)
                     preferences.edit()
-                            .putString(PREF_SSH_KEY_FILE, STORED_IN_KEYSTORE)
+                            .putString(Constants.PREF_SSH_KEY_FILE, PrivateKeyPickerPreference.STORED_IN_KEYSTORE)
                             .putString(Constants.Deprecated.PREF_SSH_KEY_PASSPHRASE, null)
-                            .apply();
-                    String message = TinyMap.of(
-                            InsideSecurityHardware.YES, "security hardware",
-                            InsideSecurityHardware.NO, "software key store",
-                            InsideSecurityHardware.CANT_TELL, "key store"
-                    ).get(isInsideSecurityHardware(SSHConnection.KEYSTORE_ALIAS));
-                    InfoToast.show("While migrating preferences, private SSH key was moved " +
-                            "into " + message);
-                } catch (Exception e) {
-                    InfoToast.show("While migrating preferences, attempted to move SSH " +
-                            "private key into AndroidKeyStore. This was unsuccessful. Reason: " +
-                            e.getMessage());
+                            .apply()
+                    val message = mapOf(
+                        InsideSecurityHardware.YES to "security hardware",
+                        InsideSecurityHardware.NO to "software key store",
+                        InsideSecurityHardware.CANT_TELL to "key store"
+                    )[AndroidKeyStoreUtils.isInsideSecurityHardware(SSHConnection.KEYSTORE_ALIAS)]
+                    showInfo("While migrating preferences, " +
+                             "private SSH key was moved into $message")
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    showError("While migrating preferences, " +
+                              "attempted to move SSH private key into AndroidKeyStore. " +
+                              "This was unsuccessful. Reason: ", e)
                 }
             }
-        }));
+        }
 
-        migrators.add(new Migrator(2, 3, () -> {
-            String sshKeyFile = preferences.getString(PREF_SSH_KEY_FILE, PREF_SSH_KEY_FILE_D);
-            String sshPassphrase = preferences.getString(Constants.Deprecated.PREF_SSH_KEY_PASSPHRASE,
-                    Constants.Deprecated.PREF_SSH_KEY_PASSPHRASE_D);
-            if (sshKeyFile == null)
-                return;
+        add(2, 3) {
+            val sshKeyFile = preferences.getString(Constants.PREF_SSH_KEY_FILE,
+                                                   Constants.PREF_SSH_KEY_FILE_D) ?: return@add
 
-            if (!STORED_IN_KEYSTORE.equals(sshKeyFile)) {
+            val sshPassphrase = preferences.getString(Constants.Deprecated.PREF_SSH_KEY_PASSPHRASE,
+                                                      Constants.Deprecated.PREF_SSH_KEY_PASSPHRASE_D)
+
+            if (PrivateKeyPickerPreference.STORED_IN_KEYSTORE != sshKeyFile) {
                 try {
-                    byte[] sshKeyFileBytes = PrivateKeyPickerPreference.getData(sshKeyFile);
-                    KeyPair keyPair = SSHConnection.makeKeyPair(sshKeyFileBytes, sshPassphrase);
+                    val sshKeyFileBytes = PrivateKeyPickerPreference.getData(sshKeyFile)
+                    val keyPair = SSHConnection.makeKeyPair(sshKeyFileBytes, sshPassphrase)
                     preferences.edit()
-                            .putString(PREF_SSH_KEY_FILE, Utils.serialize(keyPair))
-                            .apply();
-                } catch (Exception e) {
-                    InfoToast.show("Failed to migrate SSH key: " + e);
+                            .putString(Constants.PREF_SSH_KEY_FILE, Utils.serialize(keyPair))
+                            .apply()
+                } catch (e: Exception) {
+                    showError("Failed to migrate SSH key: ", e)
                     preferences.edit()
-                            .putString(PREF_SSH_KEY_FILE, PREF_SSH_KEY_FILE_D)
-                            .apply();
+                            .putString(Constants.PREF_SSH_KEY_FILE, Constants.PREF_SSH_KEY_FILE_D)
+                            .apply()
                 }
             }
 
             preferences.edit()
                     .remove(Constants.Deprecated.PREF_SSH_KEY_PASSPHRASE)
-                    .apply();
-        }));
+                    .apply()
+        }
 
-        migrators.add(new Migrator(3, 4, () -> {
-            String knownHosts = preferences.getString(Constants.Deprecated.PREF_SSH_KNOWN_HOSTS,
-                    Constants.Deprecated.PREF_SSH_KNOWN_HOSTS_D);
-            if (Constants.Deprecated.PREF_SSH_KNOWN_HOSTS_D.equals(knownHosts))
-                return;
+        add(3, 4) {
+            val knownHosts = preferences.getString(Constants.Deprecated.PREF_SSH_KNOWN_HOSTS,
+                                                   Constants.Deprecated.PREF_SSH_KNOWN_HOSTS_D)
+            if (knownHosts == Constants.Deprecated.PREF_SSH_KNOWN_HOSTS_D) return@add
 
-            InfoToast.show("While migrating preferences, the SSH known hosts preference" +
-                    " was removed. You will be prompted to accept SSH host key the next time you" +
-                    " connect using SSH.");
-
+            showInfo("While migrating preferences, the SSH known hosts preference " +
+                     "was removed. You will be prompted to accept SSH host key the next time you " +
+                     "connect using SSH.")
             preferences.edit()
                     .remove(Constants.Deprecated.PREF_SSH_KNOWN_HOSTS)
-                    .apply();
-        }));
+                    .apply()
+        }
+
+        add(4, 5) {
+            val font = preferences.getString(Constants.PREF_BUFFER_FONT,
+                                             Constants.PREF_BUFFER_FONT_D)
+            val fontSet = font != Constants.PREF_BUFFER_FONT_D
+
+            val colorSchemeDay = preferences.getString(Constants.PREF_COLOR_SCHEME_DAY,
+                                                       Constants.PREF_COLOR_SCHEME_DAY_D) ?: ""
+            val colorSchemeDaySet = colorSchemeDay.startsWith("/")
+
+            val colorSchemeNight = preferences.getString(Constants.PREF_COLOR_SCHEME_NIGHT,
+                                                         Constants.PREF_COLOR_SCHEME_NIGHT_D) ?: ""
+            val colorSchemeNightSet = colorSchemeNight.startsWith("/")
+
+            if (fontSet || colorSchemeDaySet || colorSchemeNightSet) {
+                showInfo("The app no longer requests external storage permission. " +
+                         "If you are using custom fonts or themes, " +
+                         "you may have to import them in settings.")
+            }
+        }
     }
+}
+
+
+private const val VERSION_KEY = "preferences-version-key"
+
+
+private fun showError(message: String, e: Exception) {
+    val error = FriendlyExceptions(applicationContext).getFriendlyException(e).message
+    Toaster.InfoToast.show(message + error)
+    Toaster.InfoToast.show(message + error)
+}
+
+private fun showInfo(message: String) {
+    Toaster.InfoToast.show(message)
+    Toaster.InfoToast.show(message)
 }
