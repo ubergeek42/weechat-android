a40491690ceae64efb3d17e22d3204eb900c1902
oakkitten <oakkitten@users.noreply.github.com>
2021-05-07 15:34:25 +0100

    add person to private buffer shortcuts, use compat

--- app/src/main/java/com/ubergeek42/WeechatAndroid/notifications/Shortcuts.kt
+++ app/src/main/java/com/ubergeek42/WeechatAndroid/notifications/Shortcuts.kt
@@ -2,15 +2,16 @@ package com.ubergeek42.WeechatAndroid.notifications
 
 import android.content.Context
 import android.content.Intent
-import android.content.LocusId
-import android.content.pm.ShortcutInfo
 import android.content.pm.ShortcutManager
-import android.graphics.drawable.Icon
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.core.content.LocusIdCompat
+import androidx.core.content.pm.ShortcutInfoCompat
+import androidx.core.graphics.drawable.IconCompat
 import com.ubergeek42.WeechatAndroid.WeechatActivity
 import com.ubergeek42.WeechatAndroid.relay.Buffer
 import com.ubergeek42.WeechatAndroid.relay.BufferList
+import com.ubergeek42.WeechatAndroid.relay.BufferSpec
 import com.ubergeek42.WeechatAndroid.upload.applicationContext
 import com.ubergeek42.WeechatAndroid.utils.Constants
 import com.ubergeek42.WeechatAndroid.utils.Utils
@@ -32,23 +33,29 @@ val shortcuts: ShortcutReporter = if (Build.VERSION.SDK_INT >= Build.VERSION_COD
 class Shortcuts(val context: Context): ShortcutReporter {
     private val shortcutManager = context.getSystemService(ShortcutManager::class.java)!!
 
-    private fun makeShortcutForBuffer(buffer: Buffer): ShortcutInfo {
+    private fun makeShortcutForBuffer(buffer: Buffer): ShortcutInfoCompat {
         val iconBitmap = generateIcon(text = buffer.shortName, colorKey = buffer.fullName)
-        val icon = Icon.createWithBitmap(iconBitmap)
+        val icon = IconCompat.createWithBitmap(iconBitmap)
 
         val intent = Intent(applicationContext, WeechatActivity::class.java).apply {
             putExtra(Constants.EXTRA_BUFFER_FULL_NAME, buffer.fullName)
             action = Utils.pointerToString(buffer.pointer)
         }
 
-        return ShortcutInfo.Builder(context, buffer.fullName)
+        val builder = ShortcutInfoCompat.Builder(context, buffer.fullName)
             .setShortLabel(buffer.shortName)
             .setLongLabel(buffer.shortName)
             .setIcon(icon)
             .setIntent(intent)
             .setLongLived(true)
-            .setLocusId(LocusId(buffer.fullName))
-            .build()
+            .setLocusId(LocusIdCompat(buffer.fullName))
+
+        if (buffer.type == BufferSpec.Type.Private) {
+            val person = getPerson(key = buffer.fullName, nick = buffer.shortName)
+            builder.setPerson(person)
+        }
+
+        return builder.build()
     }
 
     private val pushedShortcuts = mutableSetOf<String>()
@@ -58,7 +65,7 @@ class Shortcuts(val context: Context): ShortcutReporter {
             val key = buffer.fullName
 
             if (key !in pushedShortcuts) {
-                shortcutManager.pushDynamicShortcut(makeShortcutForBuffer(buffer))
+                shortcutManager.pushDynamicShortcut(makeShortcutForBuffer(buffer).toShortcutInfo())
                 pushedShortcuts.add(key)
             }
 
