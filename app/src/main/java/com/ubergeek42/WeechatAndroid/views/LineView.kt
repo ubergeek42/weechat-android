ubergeek42/weechat-android

commit 31ce34c2d7f16fd6d2a5b292d3cf154cab38b1c8

calculate weaselWidth individually for every activity

app/src/main/java/com/ubergeek42/WeechatAndroid/views/LineView.kt
@@ -2,6 +2,7 @@ package com.ubergeek42.WeechatAndroid.views
 
 import android.animation.ValueAnimator
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
@@ -28,7 +29,6 @@ import com.ubergeek42.WeechatAndroid.media.Engine
 import com.ubergeek42.WeechatAndroid.media.Strategy
 import com.ubergeek42.WeechatAndroid.media.WAGlideModule
 import com.ubergeek42.WeechatAndroid.relay.Line
-import com.ubergeek42.WeechatAndroid.service.P
 import com.ubergeek42.WeechatAndroid.upload.f
 import com.ubergeek42.WeechatAndroid.upload.i
 import com.ubergeek42.WeechatAndroid.upload.main
@@ -49,7 +49,6 @@ private enum class State(
     AnimatingToTextOnly(false, true, false),        // image (without image) → text. runs when the image fails to load
     AnimatingOnlyImage(true, false, true),          // narrow layout; animating only the image
 }
-
 
 class LineView @JvmOverloads constructor(
     context: Context,
@@ -211,9 +210,20 @@ class LineView @JvmOverloads constructor(
                 .coerceAtLeast(1)
     }
 
+    private var wideLayoutWidth: Int = 1000
+    private val narrowLayoutWidth get() = wideLayoutWidth - Config.thumbnailAreaWidth
+
     private var oldViewWidth = wideLayoutWidth
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val viewWidth = wideLayoutWidth
+        val parentWidth = MeasureSpec.getSize(widthMeasureSpec)
+        val viewWidth = if (parentWidth > 0) {
+            parentWidth
+        } else {
+            (context as? Activity)?.calculateApproximateWeaselWidth() ?: 1000
+        }
+
+        wideLayoutWidth = viewWidth
+
         if (oldViewWidth != viewWidth) {
             oldViewWidth = viewWidth
             wideLayoutDelegate.invalidate()
@@ -356,8 +366,6 @@ const val HAVE_NOT_DRAWN = -1L
 
 private val NoText = SpannableString("error")   // just so that we don't need to say !!
 
-private inline val wideLayoutWidth get() = P.weaselWidth
-private inline val narrowLayoutWidth get() = P.weaselWidth - Config.thumbnailAreaWidth
 
 fun View.getSafeGlide() = if (WAGlideModule.isContextValidForGlide(context)) {
                                Glide.with(context)

app/src/main/java/com/ubergeek42/WeechatAndroid/PreferencesActivity.java
@@ -31,6 +31,7 @@ import com.ubergeek42.WeechatAndroid.upload.HttpUriGetter;
 import com.ubergeek42.WeechatAndroid.upload.RequestBodyModifier;
 import com.ubergeek42.WeechatAndroid.upload.RequestModifier;
 import com.ubergeek42.WeechatAndroid.utils.Utils;
+import com.ubergeek42.WeechatAndroid.views.ViewUtilsKt;
 
 import java.util.Set;
 
@@ -252,7 +253,7 @@ public class PreferencesActivity extends AppCompatActivity implements Preference
                 } else if (PREF_MEDIA_PREVIEW_THUMBNAIL_WIDTH.equals(key)) {
                     float thumbnailWidth = P._1dp * Float.parseFloat((String) o);
                     if (thumbnailWidth <= 0) throw new IllegalArgumentException("Thumbnail width must be > 0");
-                    if (thumbnailWidth > (P.weaselWidth * 0.9)) throw new IllegalArgumentException("Thumbnail width must be less than screen width");
+                    if (thumbnailWidth > (ViewUtilsKt.calculateApproximateWeaselWidth(requireActivity()) * 0.9)) throw new IllegalArgumentException("Thumbnail width must be less than screen width");
                 } else if (PREF_MEDIA_PREVIEW_THUMBNAIL_MAX_HEIGHT.equals(key)) {
                     float thumbnailMaxHeight = P._1dp * Float.parseFloat((String) o);
                     if (thumbnailMaxHeight <= 0) throw new IllegalArgumentException("Thumbnail max height must be > 0");

app/src/main/java/com/ubergeek42/WeechatAndroid/fragments/BufferFragment.kt
@@ -73,6 +73,7 @@ import com.ubergeek42.WeechatAndroid.utils.ulet
 import com.ubergeek42.WeechatAndroid.views.BufferFragmentFullScreenController
 import com.ubergeek42.WeechatAndroid.views.OnBackGestureListener
 import com.ubergeek42.WeechatAndroid.views.OnJumpedUpWhileScrollingListener
+import com.ubergeek42.WeechatAndroid.views.calculateApproximateWeaselWidth
 import com.ubergeek42.WeechatAndroid.views.hideSoftwareKeyboard
 import com.ubergeek42.WeechatAndroid.views.jumpThenSmoothScroll
 import com.ubergeek42.WeechatAndroid.views.jumpThenSmoothScrollCentering
@@ -694,11 +695,15 @@ class BufferFragment : Fragment(), BufferEye {
             return
         }
 
+        val weaselWidth = ui.chatLines.width.let {
+            if (it > 0) it else activity?.calculateApproximateWeaselWidth() ?: 1000
+        }
+
         // for the purpose of the subsequent calculation we pretend that paperclip is shown,
         // else ratio can jump backwards on character entry, revealing the button again.
         // if the send button is off, adding a ShareSpan can reveal it (well, upload button),
         // but it's not a problem as it can only appear on text addition and disappear on deletion*
-        var widgetWidth = P.weaselWidth - P._4dp - actionButtonWidth
+        var widgetWidth = weaselWidth - P._4dp - actionButtonWidth
         if (ui.tabButton.visibility != View.GONE) widgetWidth -= actionButtonWidth
         if (ui.sendButton.visibility != View.GONE) widgetWidth -= actionButtonWidth
 

app/src/main/java/com/ubergeek42/WeechatAndroid/service/P.java
@@ -78,28 +78,8 @@ public class P implements SharedPreferences.OnSharedPreferenceChangeListener{
         new MigratePreferences(context).migrate();
         loadUIPreferences();
         p.registerOnSharedPreferenceChangeListener(instance);
-        calculateWeaselWidth();
         Config.initPreferences();
         UploadConfigKt.initPreferences();
-    }
-
-    // sets the width of weasel (effectively the recycler view) for LineView. this is a workaround
-    // necessary in order to circumvent a bug (?) in ViewPager: sometimes, when measuring views, the
-    // RecyclerView will have a width of 0 (esp. when paging through buffers fast) and hence
-    // LineView will receive a suggested maximum width of 0 in its onMeasure().
-    //      note: other views in RecyclerView don't seem to have this problem. they either receive
-    //      correct values or somehow recover from width 0. the difference seems to lie in the fact
-    //      that they are inflated, and not created programmatically.
-    // this method is called from onStart() instead of onCreate() as onCreate() is called when the
-    // activities get recreated due to theme/battery state change. for some reason, the activities
-    // get recreated even though the user is using another app; if it happens in the wrong screen
-    // orientation, the value is wrong.
-    // todo: switch to ViewPager2 and get rid of this nonsense
-    public static @Cat void calculateWeaselWidth() {
-        int windowWidth = context.getResources().getDisplayMetrics().widthPixels;
-        boolean slidy = context.getResources().getBoolean(R.bool.slidy);
-        P.weaselWidth = slidy ? windowWidth :
-                windowWidth - context.getResources().getDimensionPixelSize(R.dimen.drawer_width);
     }
 
     // set colorPrimary and colorPrimaryDark according to color scheme or app theme
@@ -137,7 +117,6 @@ public class P implements SharedPreferences.OnSharedPreferenceChangeListener{
     public static @Nullable DateTimeFormatter dateFormat;
     public static int align;
 
-    public static int weaselWidth = 1080;
     public static float textSize, letterWidth;
     public static TextPaint textPaint;
 

app/src/main/java/com/ubergeek42/WeechatAndroid/utils/WeaselMeasuringViewPager.kt
@@ -12,7 +12,9 @@ class WeaselMeasuringViewPager : ViewPagerFix {
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         measuredWidth.let {
-            if (it > 0) P.weaselWidth = it - windowInsets.left - windowInsets.right
+            if (it > 0) weaselWidth = it - windowInsets.left - windowInsets.right
         }
     }
+
+    var weaselWidth = 0
 }

app/src/main/java/com/ubergeek42/WeechatAndroid/views/ViewUtils.kt
@@ -10,14 +10,16 @@ import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import androidx.annotation.StringRes
 import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.core.view.GravityCompat
 import androidx.core.view.updateLayoutParams
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSmoothScroller
 import androidx.recyclerview.widget.RecyclerView
+import com.ubergeek42.WeechatAndroid.R
+import com.ubergeek42.WeechatAndroid.WeechatActivity
 import com.ubergeek42.WeechatAndroid.upload.applicationContext
+import com.ubergeek42.WeechatAndroid.upload.dp_to_px
 import com.ubergeek42.WeechatAndroid.upload.f
 import com.ubergeek42.WeechatAndroid.upload.i
 import com.ubergeek42.WeechatAndroid.utils.u
@@ -192,4 +194,39 @@ fun View.showSoftwareKeyboard() {
 
 fun View.hideSoftwareKeyboard() {
     imm.hideSoftInputFromWindow(this.windowToken, 0)
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// historical note from the old comment:
+// > this method is called from onStart() instead of onCreate() as onCreate() is called when the
+// > activities get recreated due to theme/battery state change. for some reason, the activities
+// > get recreated even though the user is using another app; if it happens in the wrong screen
+// > orientation, the value is wrong.
+
+// gets width of weasel (effectively the recycler view) for LineView. this is a workaround
+// necessary in order to circumvent a bug (?) in ViewPager: sometimes, when measuring views, the
+// RecyclerView will have a width of 0 (esp. when paging through buffers fast) and hence
+// LineView will receive a suggested maximum width of 0 in its onMeasure().
+//      note: other views in RecyclerView don't seem to have this problem. they either receive
+//      correct values or somehow recover from width 0. the difference seems to lie in the fact
+//      that they are inflated, and not created programmatically.
+// todo: switch to ViewPager2 and get rid of this nonsense
+fun Activity.calculateApproximateWeaselWidth(): Int {
+    if (this is WeechatActivity) {
+        ui.pager.weaselWidth.let { if (it > 0) return it }
+    }
+
+    val windowWidthIsh = window.decorView.width.let {
+        if (it > 0) it else resources.displayMetrics.widthPixels
+    }
+
+    if (this is WeechatActivity) {
+        val drawerAlwaysVisible = !resources.getBoolean(R.bool.slidy)
+        if (drawerAlwaysVisible) return windowWidthIsh - 280.dp_to_px
+    }
+
+    return windowWidthIsh
 }

commit b22aaec11e307426d966d71006f603543ec63282

LineView: clear old thumbnail request when making a new one; get a better initial wide layout width measurement in the first place
wideLayoutWidth is needed to calculate thumbnail height (wide layout ->
narrow layout -> height -> coerce). when rotating screen, setting view
text will trigger a thumbnail request. with a wrong layout width the
request can be wrong. subsequent view measurement will correct that;
however, the first wrong image can arrive after the second good one.

this fixes the issue by only ever using the latest requested image
and also by trying to avoid requesting wrong height images

this issue was introduced in 31ce34c2d7f16fd6d2a5b292d3cf154cab38b1c8

app/src/main/java/com/ubergeek42/WeechatAndroid/views/LineView.kt
@@ -78,7 +78,7 @@ class LineView @JvmOverloads constructor(
         animatedValue = 0f
         animator?.cancel()
         animator = null
-        target?.let { glide?.clear(it) }    // will call the listener!
+        glide?.clear(target)    // will call the listener!
         target = null
     }
 
@@ -139,6 +139,8 @@ class LineView @JvmOverloads constructor(
     private var lastRequestedUrl: Strategy.Url? = null
     private fun requestThumbnail(url: Strategy.Url) {
         lastRequestedUrl = url
+
+        glide?.clear(target)
 
         target = (glide ?: return)
                 .asBitmap()
@@ -210,7 +212,12 @@ class LineView @JvmOverloads constructor(
                 .coerceAtLeast(1)
     }
 
-    private var wideLayoutWidth: Int = 1000
+    private var wideLayoutWidth: Int = 0
+        get() {
+            if (field == 0) field = (context as? Activity)?.calculateApproximateWeaselWidth() ?: 1000
+            return field
+        }
+
     private val narrowLayoutWidth get() = wideLayoutWidth - Config.thumbnailAreaWidth
 
     private var oldViewWidth = wideLayoutWidth