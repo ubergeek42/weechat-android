c7742eb46a9936b914e25a0215e41ceb859cc6d1
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2021-06-13 12:08:52 +0200

    Use addLast in replaceLines
    This makes sure that lines are only added through the Lines.addLast()
    interface.

--- app/src/main/java/com/ubergeek42/WeechatAndroid/relay/Lines.kt
+++ app/src/main/java/com/ubergeek42/WeechatAndroid/relay/Lines.kt
@@ -114,9 +114,8 @@ class Lines {
         if (status != Status.Fetching) return
         unfiltered.clear()
         filtered.clear()
-        unfiltered.addAll(lines)
         for (line in lines) {
-            if (line.isVisible) filtered.add(line)
+            addLast(line)
         }
         mayHaveSquiggleLines = false
     }
