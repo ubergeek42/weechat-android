2d4c01f815c5deda6ad9e645363dd27bc3a938b2
oakkitten <oakkitten@users.noreply.github.com>
2022-06-12 21:34:42 +0100

    Clean up SimpleConnection

--- relay/src/main/java/com/ubergeek42/weechat/relay/connection/SimpleConnection.kt
+++ relay/src/main/java/com/ubergeek42/weechat/relay/connection/SimpleConnection.kt
@@ -1,8 +1,6 @@
 package com.ubergeek42.weechat.relay.connection
 
-import com.ubergeek42.weechat.relay.connection.IConnection
 import kotlin.Throws
-import com.ubergeek42.weechat.relay.connection.RelayConnection
 import java.io.IOException
 import java.net.InetSocketAddress
 import java.net.Socket
@@ -10,21 +8,22 @@ import javax.net.ssl.HostnameVerifier
 import javax.net.ssl.SSLSocketFactory
 
 class SimpleConnection(
-    private val hostname: String, private val port: Int, sslSocketFactory: SSLSocketFactory?,
-    private val verifier: HostnameVerifier
+    private val hostname: String,
+    private val port: Int,
+    sslSocketFactory: SSLSocketFactory?,
+    private val verifier: HostnameVerifier,
 ) : IConnection {
-    private val socket: Socket
-    @Throws(IOException::class) override fun connect(): IConnection.Streams {
+    private val socket = if (sslSocketFactory == null) Socket() else sslSocketFactory.createSocket()
+
+    @Throws(IOException::class)
+    override fun connect(): IConnection.Streams {
         socket.connect(InetSocketAddress(hostname, port), RelayConnection.CONNECTION_TIMEOUT)
         Utils.verifyHostname(verifier, socket, hostname)
         return IConnection.Streams(socket.getInputStream(), socket.getOutputStream())
     }
 
-    @Throws(IOException::class) override fun disconnect() {
+    @Throws(IOException::class)
+    override fun disconnect() {
         socket.close()
-    }
-
-    init {
-        socket = if (sslSocketFactory == null) Socket() else sslSocketFactory.createSocket()
     }
 }
