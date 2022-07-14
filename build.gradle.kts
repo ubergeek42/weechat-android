8c7ccb3e40fd1c6e9022c1715eb2e1c0da29fb09
oakkitten <oakkitten@users.noreply.github.com>
2022-07-14 19:00:14 +0100

    Use AGP 7.3.0-beta05 instead of pinning R8 to 3.3.62
    AGP 7.3.0-beta05 should have the same R8 fixes that we were getting by
    pulling from their own repo.
    
    Hopefully this will make F-Droid happy.

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,19 +24,17 @@ buildscript {
     repositories {
         mavenCentral()
         google()
-        maven { url = uri("https://storage.googleapis.com/r8-releases/raw") }
     }
 
     dependencies {
         // R8 below 3.1.79, 3.2.70, and 3.3.62, has an issue;
         // it tries to convert some Enums to ints but kind of stops halfway.
-        // This pin, and the above R8 repo are needed because at the moment this version
-        // is not available in maven central. TODO remove when it is
-        // https://github.com/ubergeek42/weechat-android/pull/537
+        // This was previously solved by pinning R8 to 3.3.62,
+        // but that requires Google's own R8 repo as this version wasn't on Maven,
+        // which is an issue since F-Droid decided to not allowlist that repo.
+        // Instead, we now use Android Gradle Plugin 7.3.0-beta05, which has a fix.
         // https://issuetracker.google.com/issues/236618700
-        classpath("com.android.tools:r8:3.3.62")
-
-        classpath("com.android.tools.build:gradle:7.2.1")
+        classpath("com.android.tools.build:gradle:7.3.0-beta05")
         classpath("org.aspectj:aspectjtools:1.9.9.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.7.0")
