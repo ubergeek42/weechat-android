3a8f7a96a4022ccd11a5be3959ae0ef9eca74f72
oakkitten <oakkitten@users.noreply.github.com>
2024-03-09 17:11:46 +0000

    Do not try to emojify using `EmojiCompat` if load failed
    We used to try emojifying regardless of EmojiCompat load state. This was not
    a big issue, as `EmojiCompat` usually loads fast enough. However, on some
    systems loading dynamic fonts is not available, which resulted in errors
    printed for every emojification attempt, i. e. for every single line.
    
    This change makes emojifications occur only when `EmojiCompat` is fully
    initialized and loaded. Also, initialization status is printed to logcat.
    
    Closes #561

--- app/src/main/java/com/ubergeek42/WeechatAndroid/utils/EmojiUtil.kt
+++ app/src/main/java/com/ubergeek42/WeechatAndroid/utils/EmojiUtil.kt
@@ -3,42 +3,63 @@ package com.ubergeek42.WeechatAndroid.utils
 import android.annotation.SuppressLint
 import android.os.Build
 import android.text.Spannable
+import androidx.emoji2.text.DefaultEmojiCompatConfig
 import androidx.emoji2.text.EmojiCompat
-import com.ubergeek42.WeechatAndroid.upload.suppress
-import java.lang.IllegalStateException
+import androidx.emoji2.text.EmojiCompat.InitCallback
+import androidx.emoji2.text.EmojiCompat.LOAD_STRATEGY_MANUAL
+import androidx.emoji2.text.FontRequestEmojiCompatConfig.ExponentialBackoffRetryPolicy
+import com.ubergeek42.WeechatAndroid.upload.applicationContext
+import com.ubergeek42.cats.Kitty
+import com.ubergeek42.cats.Root
+import kotlin.concurrent.thread
+
+@Root private val kitty = Kitty.make("EmojiUtil")
 
 
 // Android 12 and above can update emoji font at any time, while 11 and below
 // only receives emoji updates along with system updates.
 // AppCompat 1.4+ automatically adds support for new emoji for its views,
 // but custom views should process the text manually.
-val SHOULD_EMOJIFY = Build.VERSION.SDK_INT < Build.VERSION_CODES.S
+@JvmField val SHOULD_EMOJIFY = Build.VERSION.SDK_INT < Build.VERSION_CODES.S
 
 
-// Must be called after EmojiCompat.init(), which is normally called by
-// EmojiCompatInitializer around application OnCreate.
-private val emojiCompat = EmojiCompat.get()
+// See https://developer.android.com/develop/ui/views/text-and-emoji/emoji2#optional-features
+fun initEmojiCompat() {
+    val config = DefaultEmojiCompatConfig.create(applicationContext)
 
-
-// Add spans with missing emojis.
-// Calling process() returns the given input if it is Spannable.
-//
-// While we can always get an EmojiCompat instance after initializing it,
-// it may take some time to load the font, and in some rare cases
-// the app will want to emojify before the font has loaded. While in such a state,
-// calling process() will throw IllegalStateException("Not initialized yet").
-//
-// This can be triggered by:
-//   * opening a buffer
-//   * minimizing and killing the app
-//   * starting the app by tapping on its icon
-//
-// There are workarounds, but the above state occurs rarely,
-// and the impact of not having the latest emojis is rather minimal.
-// So, simply suppress the exception.
-@SuppressLint("CheckResult")
-fun emojify(text: Spannable) {
-    suppress<IllegalStateException> {
-        emojiCompat.process(text)
+    if (config == null) {
+        kitty.error("Could not create DefaultEmojiCompatConfig: no font provider found")
+        return
     }
+
+    var emojiCompat: EmojiCompat? = null
+
+    config.setRetryPolicy(ExponentialBackoffRetryPolicy(5 * 60 * 1000))
+    config.setMetadataLoadStrategy(LOAD_STRATEGY_MANUAL)
+    config.registerInitCallback(object : InitCallback() {
+        override fun onInitialized() {
+            kitty.info("EmojiCompat initialized")
+            emojiCompatOrNull = emojiCompat
+        }
+
+        override fun onFailed(throwable: Throwable?) {
+            kitty.error("EmojiCompat initialization failed", throwable)
+        }
+    })
+
+    emojiCompat = EmojiCompat.init(config)
+
+    thread { emojiCompat.load() }
+}
+
+
+// Fully initialized and loaded EmojiCompat,
+// or null if still initializing or if no suitable font providers found.
+@Volatile private var emojiCompatOrNull: EmojiCompat? = null
+
+
+// Replace missing emojis with modern emojis
+@SuppressLint("CheckResult") // Calling process() returns the given input if it is Spannable.
+fun emojify(text: Spannable) {
+    emojiCompatOrNull?.process(text)
 }
