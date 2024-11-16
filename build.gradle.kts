ed0c544dc6c6e90a9352113302ae6c5075d01c24
oakkitten <oakkitten@users.noreply.github.com>
2024-11-16 21:16:01 +0000

    Fix some lint warnings in gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,7 +78,7 @@ plugins {
 }
 
 fun isNonStable(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
     val regex = "^[0-9,.v-]+(-r)?$".toRegex()
     val isStable = stableKeyword || regex.matches(version)
     return isStable.not()
