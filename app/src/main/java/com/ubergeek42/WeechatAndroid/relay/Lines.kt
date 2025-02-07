0ca58db9c01ea075f6b1f37de9c9d4583dd517f8
oakkitten <oakkitten@users.noreply.github.com>
2025-02-07 21:50:40 +0000

    Fix bad behavior when `Lines.replaceLine()` reveals a hidden line
    Also add some tests for the method and other around it.
    
    This moves the logic of adding a squiggle line from `addLast()` to
    `updateLastLineInfo()`, simplifying things, however, now in some cases the
    squiggle lines might become doubled. This also allows, or rather simplifies,
    having squiggle lines that are properly `isVisible`, which helps with
    preserving them when converting from `unfiltered` to `filtered`. This
    conversion becomes wanted as with `replaceLine()` some of the lines may become
    visible.
    
    For the same reason, `replaceLine()` also sets skips now.
    
    This also removes `skipFilteredOffset` and `skipUnfilteredOffset` as I just
    could not figure out what these were for. Should've added a comment. Might
    simply be obsolete logic that stayed due to poor refactoring.
    
    Overall, this is a dangerous change for it touches complex code that has
    been working well for quite some time. Ideally it should be rewritten in
    some way. This could take a lot of effort and testing.
    
    P.S. The immediate problem that prompted this change was the following crash:
    
    	Exception java.util.NoSuchElementException: ArrayDeque is empty.
    	  at kotlin.collections.ArrayDeque.removeFirst (SourceFile:38)
    	  at com.ubergeek42.WeechatAndroid.relay.Lines.addLast (SourceFile:124)
    	  at com.ubergeek42.WeechatAndroid.relay.BufferList$$ExternalSyntheticLambda0.handleMessage$com$ubergeek42$WeechatAndroid$relay$BufferList$$ExternalSyntheticLambda5 (SourceFile:91)
    	  at com.ubergeek42.WeechatAndroid.relay.BufferList$$ExternalSyntheticLambda0.handleMessage (SourceFile:138)
    	  at com.ubergeek42.weechat.relay.connection.RelayConnection$$ExternalSyntheticLambda7.run (SourceFile:111)
    	  at androidx.core.app.ActivityCompat$$ExternalSyntheticLambda0.run (SourceFile:33)
    	  at com.ubergeek42.weechat.relay.connection.Utils$FriendlyThread.run (SourceFile:8)
    
    This happened at the end of the line,
    
        if (unfiltered.removeFirst().isVisible) filtered.removeFirst()
    
    Here, `filtered` can be empty even if `unfiltered` has a visible line. This
    happens if smart filter unhides a join after a message in an otherwise
    empty buffer.

--- app/src/main/java/com/ubergeek42/WeechatAndroid/relay/Lines.kt
+++ app/src/main/java/com/ubergeek42/WeechatAndroid/relay/Lines.kt
@@ -30,8 +30,6 @@ class Lines {
             headerLineDelegate.invalidate()
             if (value == Status.Init) {
                 maxUnfilteredSize = P.lineIncrement
-                shouldAddSquiggleOnNewLine = false
-                shouldAddSquiggleOnNewVisibleLine = false
             }
         }
 
@@ -40,8 +38,6 @@ class Lines {
 
     private var skipUnfiltered = -1
     private var skipFiltered = -1
-    private var skipUnfilteredOffset = -1
-    private var skipFilteredOffset = -1
 
     var maxUnfilteredSize = P.lineIncrement
         private set
@@ -49,24 +45,17 @@ class Lines {
     // after reconnecting, in *full sync mode*, we are receiving and adding new lines to buffers.
     // there can be inconsistencies; if some lines were added while we were offline,
     // we can't display them, but can display what's on top and what's on bottom.
-    // to resolve this, add a squiggly separator between the old and the new lines ...
-    private var shouldAddSquiggleOnNewLine = false
-    private var shouldAddSquiggleOnNewVisibleLine = false
-
-
-    // ... but only add it if the buffer's last line pointer has changed.
-    // do it separately for visible and invisible lines; note, however,
-    // that the list of lines is likely incomplete, and the pointer to the last line may be invalid
-
-    // also note that some lines might have changed visibility due to e.g. smart_filter;
-    // but keeping them hidden would not create an inconsistency due to their nature
+    // to resolve this, add a squiggly separator between the old and the new lines,
+    // but only add it if the buffer's last line pointer has changed.
     fun updateLastLineInfo(lastPointerServer: Long?, lastVisiblePointerServer: Long?) {
         val lastPointer = unfiltered.lastOrNull()?.pointer
         val lastVisiblePointer = filtered.lastOrNull()?.pointer
 
-        if (lastPointerServer != lastPointer) shouldAddSquiggleOnNewLine = true
-        if (shouldAddSquiggleOnNewLine && lastVisiblePointerServer != lastVisiblePointer)
-                    shouldAddSquiggleOnNewVisibleLine = true
+        if (lastPointer != null && lastPointerServer != lastPointer) {
+            val squiggleIsVisible = lastVisiblePointerServer != lastVisiblePointer
+            val squiggleLine = SquiggleLine(isVisible = squiggleIsVisible)
+            addLast(squiggleLine)
+        }
     }
 
     // it might look like there's a room for optimization here,
@@ -78,7 +67,10 @@ class Lines {
             val skip = if (P.filterLines) skipFiltered else skipUnfiltered
             val marker = if (skip >= 0 && size > 0) size - skip else -1
             if (marker > 0) add(marker, MarkerLine)
+
+            while (isNotEmpty() && first() is SquiggleLine) removeFirst()
             while (isNotEmpty() && last() is SquiggleLine) removeLast()
+            // TODO remove consecutive squiggles?
         }
     }
 
@@ -99,27 +91,12 @@ class Lines {
 
     fun replaceLine(line: Line) {
         unfiltered.replaceFirstWith(line, predicate = { it.pointer == line.pointer })
-        filtered.replaceFirstWith(line, predicate = { it.pointer == line.pointer })
+        filtered.clear()
+        unfiltered.forEach { line -> if (line.isVisible) filtered.add(line) }
+        setSkipsUsingPointer()
     }
 
     fun addLast(line: Line) {
-        if (shouldAddSquiggleOnNewLine) {
-            shouldAddSquiggleOnNewLine = false
-            if (status == Status.Init && unfiltered.size > 0) addLast(SquiggleLine())   // invisible
-        }
-
-        if (shouldAddSquiggleOnNewVisibleLine && line.isVisible) {
-            shouldAddSquiggleOnNewVisibleLine = false
-            if (status == Status.Init && filtered.size > 0) {
-                // “unhide” the squiggly line added above. as it's hidden,
-                // the size of visible lines is surely less than maximum
-                unfiltered.reversed().firstOrNull { it is SquiggleLine }?.let {
-                    filtered.addLast(it)
-                    if (skipFiltered >= 0) skipFiltered++
-                }
-            }
-        }
-
         val unfilteredSize = unfiltered.size
         val maxUnfilteredSize = maxUnfilteredSize
         val shouldRemoveFirstLine = unfilteredSize == maxUnfilteredSize
@@ -192,25 +169,11 @@ class Lines {
     ////////////////////////////////////////////////////////////////////////////////////////////////
 
     fun moveReadMarkerToEnd() {
-        if (skipFilteredOffset >= 0 && skipUnfilteredOffset >= 0 &&
-                skipFiltered >= skipFilteredOffset && skipUnfiltered >= skipFilteredOffset) {
-            skipFiltered -= skipFilteredOffset
-            skipUnfiltered -= skipUnfilteredOffset
-        } else {
-            skipUnfiltered = 0
-            skipFiltered = 0
-        }
-        skipUnfilteredOffset = -1
-        skipFilteredOffset = -1
-    }
+        skipUnfiltered = 0
+        skipFiltered = 0
 
-    fun rememberCurrentSkipsOffset() {
-        skipFilteredOffset = skipFiltered
-        skipUnfilteredOffset = skipUnfiltered
-        if (unfiltered.size > 0) _lastSeenLine = unfiltered.last().pointer
+        if (unfiltered.isNotEmpty()) _lastSeenLine = unfiltered.last().pointer
     }
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////
 
     private var _lastSeenLine = -1L
     var lastSeenLine: Long
@@ -255,15 +218,15 @@ private var fakePointerCounter = MAX_C_POINTER_VALUE
 private val TITLE_LINE_POINTER = ++fakePointerCounter
 
 
-open class FakeLine(pointer: Long) : Line(
+open class FakeLine(pointer: Long, isVisible: Boolean = false) : Line(
         pointer, LineSpec.Type.Other,
         timestamp = 0, rawPrefix = "", rawMessage = "",
-        nick = null, isVisible = false, isHighlighted = false,
+        nick = null, isVisible = isVisible, isHighlighted = false,
         LineSpec.DisplayAs.Unspecified, LineSpec.NotifyLevel.Low)
 
 
-object MarkerLine : FakeLine(++fakePointerCounter)              // can have only one per buffer
-class SquiggleLine : FakeLine(++fakePointerCounter)             // can have several of these
+object MarkerLine : FakeLine(++fakePointerCounter)  // can have only one per buffer
+class SquiggleLine(pointer: Long = ++fakePointerCounter, isVisible: Boolean = false) : FakeLine(pointer, isVisible)  // can have several of these
 
 
 class HeaderLine(
--- app/src/testDebug/java/com/ubergeek42/WeechatAndroid/relay/LinesTest.kt
+++ app/src/testDebug/java/com/ubergeek42/WeechatAndroid/relay/LinesTest.kt
@@ -4,6 +4,7 @@ import androidx.test.core.app.ApplicationProvider
 import com.ubergeek42.WeechatAndroid.service.P
 import com.ubergeek42.WeechatAndroid.utils.applicationContext
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
@@ -54,5 +55,242 @@ class LinesTest {
         P.init(applicationContext)
         P.loadConnectionPreferences()
         P.filterLines = false
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Test fun `Squiggle line appears in both unfiltered and filtered modes`() {
+        val lines = Lines()
+        lines.addLast(makeLine(0))
+        lines.updateLastLineInfo(lastPointerServer = 1, lastVisiblePointerServer = 1)
+        lines.addLast(makeLine(2))
+
+        lines.assertConformsTo(
+            hasPointer(0),
+            isSquiggle(),
+            hasPointer(2)
+        )
+
+        P.filterLines = true
+
+        lines.assertConformsTo(
+            hasPointer(0),
+            isSquiggle(),
+            hasPointer(2)
+        )
+    }
+
+    @Test fun `Squiggle line appears only in unfiltered mode`() {
+        val lines = Lines()
+        lines.addLast(makeLine(0))
+        lines.updateLastLineInfo(lastPointerServer = 1, lastVisiblePointerServer = 0)
+        lines.addLast(makeLine(2))
+
+        lines.assertConformsTo(
+            hasPointer(0),
+            isSquiggle(),
+            hasPointer(2)
+        )
+
+        P.filterLines = true
+
+        lines.assertConformsTo(
+            hasPointer(0),
+            hasPointer(2),
+        )
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Test fun `replaceLine() works for lines before unfiltered & filtered squiggle`() {
+        val lines = Lines()
+        lines.addLast(makeLine(0))
+        lines.addLast(makeLine(1, visible = false))
+        lines.updateLastLineInfo(lastPointerServer = 2, lastVisiblePointerServer = 2)
+        lines.addLast(makeLine(3))
+
+        P.filterLines = true
+
+        lines.assertConformsTo(
+            hasPointer(0),
+            isSquiggle(),
+            hasPointer(3)
+        )
+
+        lines.replaceLine(makeLine(1, visible = true))
+
+        lines.assertConformsTo(
+            hasPointer(0),
+            hasPointer(1),
+            isSquiggle(),
+            hasPointer(3)
+        )
+
+        lines.replaceLine(makeLine(1, visible = false))
+
+        lines.assertConformsTo(
+            hasPointer(0),
+            isSquiggle(),
+            hasPointer(3)
+        )
+    }
+
+    // Not considering here the scenario where
+    // updateLastLineInfo is called with lastVisiblePointerServer = 1.
+    @Test fun `replaceLine() works for lines before unfiltered-only squiggle`() {
+        val lines = Lines()
+        lines.addLast(makeLine(0))
+        lines.addLast(makeLine(1, visible = false))
+        lines.updateLastLineInfo(lastPointerServer = 2, lastVisiblePointerServer = 0)
+        lines.addLast(makeLine(3))
+
+        lines.assertConformsTo(
+            hasPointer(0),
+            hasPointer(1),
+            isSquiggle(),
+            hasPointer(3)
+        )
+
+        P.filterLines = true
+
+        lines.assertConformsTo(
+            hasPointer(0),
+            hasPointer(3)
+        )
+
+        lines.replaceLine(makeLine(1, visible = true))
+
+        lines.assertConformsTo(
+            hasPointer(0),
+            hasPointer(1),
+            hasPointer(3)
+        )
+
+        lines.replaceLine(makeLine(1, visible = false))
+
+        lines.assertConformsTo(
+            hasPointer(0),
+            hasPointer(3)
+        )
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Test fun `replaceLine() works for lines after unfiltered & filtered squiggle`() {
+        val lines = Lines()
+        lines.addLast(makeLine(0))
+        lines.addLast(makeLine(1))
+        lines.updateLastLineInfo(lastPointerServer = 2, lastVisiblePointerServer = 2)
+        lines.addLast(makeLine(3, visible = false))
+
+        P.filterLines = true
+
+        lines.assertConformsTo(
+            hasPointer(0),
+            hasPointer(1),
+        )
+
+        lines.replaceLine(makeLine(3, visible = true))
+
+        lines.assertConformsTo(
+            hasPointer(0),
+            hasPointer(1),
+            isSquiggle(),
+            hasPointer(3)
+        )
+
+        lines.replaceLine(makeLine(3, visible = false))
+
+        lines.assertConformsTo(
+            hasPointer(0),
+            hasPointer(1),
+        )
+    }
+
+    @Test fun `replaceLine() works for lines after unfiltered-only squiggle`() {
+        val lines = Lines()
+        lines.addLast(makeLine(0))
+        lines.addLast(makeLine(1))
+        lines.updateLastLineInfo(lastPointerServer = 2, lastVisiblePointerServer = 1)
+        lines.addLast(makeLine(3, visible = false))
+
+        P.filterLines = true
+
+        lines.assertConformsTo(
+            hasPointer(0),
+            hasPointer(1),
+        )
+
+        lines.replaceLine(makeLine(3, visible = true))
+
+        lines.assertConformsTo(
+            hasPointer(0),
+            hasPointer(1),
+            hasPointer(3)
+        )
+
+        lines.replaceLine(makeLine(3, visible = false))
+
+        lines.assertConformsTo(
+            hasPointer(0),
+            hasPointer(1),
+        )
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    @Test fun `replaceLine() works sensibly with read marker`() {
+        val lines = Lines()
+        lines.addLast(makeLine(0))
+        lines.addLast(makeLine(1, visible = false))
+
+        lines.assertConformsTo(
+            hasPointer(0),
+            hasPointer(1),
+        )
+
+        lines.moveReadMarkerToEnd()
+
+        lines.assertConformsTo(
+            hasPointer(0),
+            hasPointer(1),
+            isReadMarker(),
+        )
+
+        P.filterLines = true
+
+        lines.assertConformsTo(
+            hasPointer(0),
+            isReadMarker(),
+        )
+
+        lines.addLast(makeLine(2, visible = false))
+        lines.addLast(makeLine(3, visible = true))
+
+        lines.assertConformsTo(
+            hasPointer(0),
+            isReadMarker(),
+            hasPointer(3),
+        )
+
+        lines.replaceLine(makeLine(1, visible = true))
+
+        lines.assertConformsTo(
+            hasPointer(0),
+            hasPointer(1),
+            isReadMarker(),
+            hasPointer(3),
+        )
+
+        lines.replaceLine(makeLine(2, visible = true))
+
+        lines.assertConformsTo(
+            hasPointer(0),
+            hasPointer(1),
+            isReadMarker(),
+            hasPointer(2),
+            hasPointer(3),
+        )
     }
 }
