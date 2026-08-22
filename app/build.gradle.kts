fb2b0ab3002974da8ad2bd6fc281e90f4168a9b1
oakkitten <oakkitten@users.noreply.github.com>
2026-08-22 22:43:31 +0100

    Fix Robolectric

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -119,6 +119,22 @@ android {
 
     testOptions {
         unitTests.isIncludeAndroidResources = true
+
+        // Needed to run Robolectric on Java 17 or higher
+        // See https://robolectric.org/getting-started/#running-with-java-17-and-higher
+        unitTests.all {
+            it.jvmArgs(
+                "--add-opens=java.base/java.lang=ALL-UNNAMED",
+                "--add-opens=java.base/java.util=ALL-UNNAMED",
+                "--add-opens=java.base/java.io=ALL-UNNAMED",
+                "--add-opens=java.base/java.net=ALL-UNNAMED",
+                "--add-opens=java.base/java.security=ALL-UNNAMED",
+                "--add-opens=java.base/java.text=ALL-UNNAMED",
+                "--add-opens=java.base/jdk.internal.access=ALL-UNNAMED",
+                "--add-opens=java.desktop/java.awt.font=ALL-UNNAMED",
+                "--add-opens=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED",
+            )
+        }
     }
 }
 
